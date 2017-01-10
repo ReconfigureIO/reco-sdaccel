c6ae1c9fa1c6e149bed39f875a5be95e916841ba
zynaptic <chris@zynaptic.com>
2017-01-10 14:15:17 +0000

    Verilog fixes for verilator lint process
    Further fixes for verilator lint warnings

--- go-teak/sdaccel/verilog/action_reset_handler.v
+++ go-teak/sdaccel/verilog/action_reset_handler.v
@@ -5,15 +5,15 @@
 //
 
 //
-// Implementation of action entity reset handler with Donut reset request
-// handshake. Supports a configurable initial reset duty cycle and subsequent 
-// sequencing of a configurable number of reset domains.
+// Implementation of action entity reset handler with system reset request. 
+// Supports a configurable initial reset duty cycle and subsequent sequencing 
+// of a configurable number of reset domains.
 //
 
 `timescale 1ns/1ps
 
 module action_reset_handler
-  (donutRstReq, donutRstDone, domainRst, domainRdy, clk);
+  (sysRstReq, domainRst, domainRdy, clk);
   
 // Specifies the number of clocks for initial reset duty cycle.
 parameter ResetDutyCycle = 15;
@@ -27,9 +27,8 @@ parameter ResetDomains = 1;
 // Specifies the clock input. There is no system level reset.
 input clk;
 
-// Specifies the Donut reset handshake signals.
-input  donutRstReq;
-output donutRstDone;
+// Specifies the system reset request signal.
+input  sysRstReq;
 
 // Specifies the reset domain reset and ready vectors. The reset domain 
 // connected to the lines at index zero will be released from reset first.
@@ -50,8 +49,8 @@ reg [ResetDomains-1:0] readyState_d;
 reg [ResetDomains-1:0] readyState_q;
 
 // Specifies the Donut reset done output register.
-reg donutResetDone_d;
-reg donutResetDone_q;
+reg sysResetDone_d;
+reg sysResetDone_q;
 
 // Implements a register with an explicit initialisation value, which will have
 // the effect of holding the resets active immediately after loading the FPGA
@@ -62,25 +61,25 @@ reg resetHandlerEnabled_q = 1'b0;
 integer i;
 
 // Implement combinatorial logic.
-always @(domainRdy, resetCounter_q, resetState_q, readyState_q, donutResetDone_q)
+always @(domainRdy, resetCounter_q, resetState_q, readyState_q, sysResetDone_q)
   begin
         
     // Hold current state by default.
     resetCounter_d = resetCounter_q;
     resetState_d = resetState_q;
     readyState_d = readyState_q;
-    donutResetDone_d = donutResetDone_q;
+    sysResetDone_d = sysResetDone_q;
         
     // Process reset sequencing after initial reset duty cycle.
     if (resetCounter_q == 0)
     begin
       readyState_d = readyState_q | domainRdy;
       resetState_d[0] = 1'b0;
-      donutResetDone_d = readyState_q[0];
+      sysResetDone_d = readyState_q[0];
       for (i = 1; i < ResetDomains; i = i + 1)
       begin
         resetState_d[i] = ~readyState_q[i-1];
-        donutResetDone_d = donutResetDone_d & readyState_q[i];
+        sysResetDone_d = sysResetDone_d & readyState_q[i];
       end
     end     
     
@@ -93,25 +92,24 @@ always @(domainRdy, resetCounter_q, resetState_q, readyState_q, donutResetDone_q
 // reset sequencing.
 always @(posedge clk)
   begin
-    if (donutRstReq)
+    if (sysRstReq)
     begin
       resetCounter_q <= ResetDutyCycle;
       resetState_q <= {(ResetDomains){1'b1}};;
       readyState_q <= {(ResetDomains){1'b0}};;
-      donutResetDone_q <= 1'b0;
+      sysResetDone_q <= 1'b0;
       resetHandlerEnabled_q <= 1'b1;
     end
-    else if (resetHandlerEnabled_q & ~donutResetDone_q)
+    else if (resetHandlerEnabled_q & ~sysResetDone_q)
     begin
       resetCounter_q <= resetCounter_d;
       resetState_q <= resetState_d;
       readyState_q <= readyState_d;
-      donutResetDone_q <= donutResetDone_d;
+      sysResetDone_q <= sysResetDone_d;
       resetHandlerEnabled_q <= 1'b1;
     end
   end
   
-assign donutRstDone = donutResetDone_q;
 assign domainRst = resetState_q;
   
 endmodule
