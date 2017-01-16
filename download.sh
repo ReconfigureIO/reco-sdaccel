82798780b94f636ca1c3dc1d1ff730a6e6368915
Josh Bohde <josh@joshbohde.com>
2017-01-16 13:52:53 -0600

    Move downloads to makefile.

--- download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-SDACCEL_WRAPPER_VERSION=PR-165
-
-aws s3 cp "s3://nerabus/eTeak/eTeak-$SDACCEL_WRAPPER_VERSION-linux-x86_64-release.tar.gz" .
-rm -rf sdaccel || true
-tar -xf "eTeak-$SDACCEL_WRAPPER_VERSION-linux-x86_64-release.tar.gz"
