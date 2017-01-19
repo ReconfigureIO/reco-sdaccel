37c110f67554b8d5fbbcd35230aa251dd82c36b3
Josh Bohde <josh@joshbohde.com>
2017-01-19 12:02:04 -0600

    Move verilog building to the makefile.

--- reco-sdaccel.mk
+++ reco-sdaccel.mk
@@ -2,6 +2,7 @@
 BUILD_DIR := "$(ROOT_DIR)/.reco-work/sdaccel/build"
 DIST_DIR := "$(ROOT_DIR)/.reco-work/sdaccel/dist"
 XCLBIN_DIR := "$(ROOT_DIR)/.reco-work/sdaccel/dist/xclbin"
+VERILOG_DIR := "$(ROOT_DIR)/.reco-work/sdaccel/verilog"
 
 XO_NAME := "reconfigure_io_reco_sdaccel_stub_0_1.xo"
 
@@ -9,11 +10,13 @@ KERNEL_NAME := "kernel_test"
 DEVICE := "xilinx_adm-pcie-ku3_2ddr-xpr_3_2"
 TARGET := "hw_emu"
 
-.PHONY: kernel xo clean
+.PHONY: kernel xo clean verilog
 
 kernel: ${XCLBIN_DIR}/${KERNEL_NAME}.${TARGET}.${DEVICE}.xclbin
 
 xo: ${BUILD_DIR}/${XO_NAME}
+
+verilog: ${VERILOG_DIR}/main.v
 
 ${BUILD_DIR}:
 	mkdir -p "${BUILD_DIR}"
@@ -26,6 +29,12 @@ ${XCLBIN_DIR}:
 
 ${XCLBIN_DIR}/${KERNEL_NAME}.${TARGET}.${DEVICE}.xclbin: ${BUILD_DIR}/${XO_NAME} ${XCLBIN_DIR}
 	xocc -j8 -O3 -t "${TARGET}" --xdevice 'xilinx:adm-pcie-ku3:2ddr-xpr:3.2' -l $< -o $@
+
+${VERILOG_DIR}:
+	mkdir -p "${VERILOG_DIR}"
+
+${VERILOG_DIR}/main.v: ${VERILOG_DIR}
+	cd ${DIR}/eTeak && PATH=${DIR}/eTeak/bin:${PATH} GOPATH=${DIR}/go-teak ./go-teak-sdaccel build ${INPUT_FILE} -o $@
 
 clean:
 	rm -rf "${ROOT_DIR}/.reco-work"
