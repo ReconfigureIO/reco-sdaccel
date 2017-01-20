9e44a37fe57d0786b6977e3d66afa15fb56fb50a
Josh Bohde <josh@joshbohde.com>
2017-01-20 14:37:59 -0600

    fix env.

--- workflows/build.sh
+++ workflows/build.sh
@@ -2,10 +2,12 @@ set -ex
 
 export LC_ALL=C
 export XILINX_SDACCEL=/opt/Xilinx/SDx/2016.3
+export XILINX_SDX="$XILINX_SDACCEL"
 export XILINX_VIVADO="$XILINX_SDACCEL/Vivado"
 export XILINXD_LICENSE_FILE=2100@l3976.local.nimbix.net
 export RECO_PATH="/data/reco/$VERSION"
-export PATH="$XILINX_SDACCEL/bin:$RECO_PATH:$XILINX_VIVADO/bin":$PATH
+export GOROOT="$RECO_PATH/go-root"
+export PATH="$GOROOT/bin:$XILINX_SDACCEL/bin:$RECO_PATH:$XILINX_VIVADO/bin":$PATH
 
 JOB=$(uuidgen -t)
 mkdir -p "/data/tests/$JOB"
