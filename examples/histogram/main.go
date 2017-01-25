da8dab60b6b49bea86815ea1056acdb05af47783
Max Siegieda <maxsiegieda@gmail.com>
2017-01-25 10:32:46 +0000

    histogram scaffolding

---
+++ examples/histogram/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	// import the entire framework (including bundled verilog)
+	"sdaccel"
+
+	"sdaccel/control"
+)
+
+// magic identifier for exporting
+func Top(
+	controlReadAddr <-chan control.Addr,
+	controlReadData chan<- control.ReadData,
+	controlWriteAddr <-chan control.Addr,
+	controlWriteData <-chan control.WriteData,
+	controlResp chan<- control.Resp) {
+}
