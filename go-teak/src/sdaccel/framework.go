d248b1ac1e0bcc7a1aa294a5dbef3df3281f2272
Josh Bohde <josh@joshbohde.com>
2017-01-27 15:40:22 -0600

    use gmem wrapper.

--- go-teak/src/sdaccel/framework.go
+++ go-teak/src/sdaccel/framework.go
@@ -3,7 +3,7 @@ package sdaccel
 // #include verilog/action_reset_handler.v
 // #include verilog/sda_kernel_ctrl_reg_sel.v
 // #include verilog/sda_kernel_ctrl_reg.v
-// #include verilog/sda_kernel_wrapper_nomem.v
+// #include verilog/sda_kernel_wrapper_gomem.v
 
 func init() {
 }
