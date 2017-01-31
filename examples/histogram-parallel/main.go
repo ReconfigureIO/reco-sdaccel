1b44f47a5284c59b0e3b4a264a5b7367a56129f0
Josh Bohde <josh@joshbohde.com>
2017-01-31 14:41:28 -0600

    update other examples to use new control interface.

--- examples/histogram-parallel/main.go
+++ examples/histogram-parallel/main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	// import the entire framework (including bundled verilog)
-	"sdaccel"
-
-	"sdaccel/control"
+	_ "sdaccel"
 )
 
 // magic identifier for exporting
 func Top(
-	controlReadAddr <-chan control.Addr,
-	controlReadData chan<- control.ReadData,
-	controlWriteAddr <-chan control.Addr,
-	controlWriteData <-chan control.WriteData,
-	controlResp chan<- control.Resp) {
+	controlAddr chan<- uint32,
+	controlData <-chan uint32) {
 }
