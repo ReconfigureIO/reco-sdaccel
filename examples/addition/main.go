7962da2db19343d20c09ebfc5801f19de80e360f
Josh Bohde <josh@joshbohde.com>
2017-02-02 15:30:55 -0600

    update addition.

--- examples/addition/main.go
+++ examples/addition/main.go
@@ -3,21 +3,54 @@ package main
 import (
 	// import the entire framework (including bundled verilog)
 	_ "sdaccel"
-)
 
-func add(x uint32, y uint32) uint32 {
-	return x + y
-}
+	"sdaccel/control"
+	"sdaccel/memory"
+)
 
 // magic identifier for exporting
 func Top(
 	controlAddr chan<- uint32,
-	controlData <-chan uint32) {
+	controlData <-chan uint32,
+
+	memReadAddr chan<- memory.Addr,
+	memReadData <-chan memory.ReadData,
+
+	memWriteAddr chan<- memory.Addr,
+	memWriteData chan<- memory.WriteData,
+	memResp <-chan memory.Response,
+
+	controlReadAddr <-chan control.Addr,
+	controlReadData chan<- control.ReadData,
+
+	controlWriteAddr <-chan control.Addr,
+	controlWriteData <-chan control.WriteData,
+	controlResp chan<- control.Resp) {
+
+	// Disable AXI-Lite control accesses.
+	go control.DisableReads(controlReadAddr, controlReadData)
+	go control.DisableWrites(controlWriteAddr, controlWriteData, controlResp)
+
+	// Disable AXI memory accesses.
+	go memory.DisableReads(memReadAddr, memReadData)
 
 	read := func(a uint32) uint32 {
 		controlAddr <- a
 		return <-controlData
 	}
 
-	println(add(read(0), read(1)))
+	val := read(0) + read(1)
+	addr := uint64(read(2))<<32 | uint64(read(3))
+
+	memWriteAddr <- memory.Addr{
+		Addr: addr,
+		Len:  1,
+	}
+
+	memWriteData <- memory.WriteData{
+		Data: val,
+		Strb: [4]bool{1, 1, 1, 1},
+	}
+
+	<-memResp
 }
