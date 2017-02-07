24af90d75bc883dfca8c6657227527f8c504a998
Max Siegieda <maxsiegieda@gmail.com>
2017-02-07 16:55:11 +0000

    make read and write buffers, pass as args to FPGA

--- examples/memcopy/cmd/test-memcopy/main.go
+++ examples/memcopy/cmd/test-memcopy/main.go
@@ -15,17 +15,21 @@ func main() {
 	krnl := world.Import("kernel_test").GetKernel("reconfigure_io_reco_sdaccel_stub_0_1")
 	defer krnl.Release()
 
-	buff := world.Malloc(xcl.WriteOnly, 4)
-	defer buff.Free()
+	writeBuff := world.Malloc(xcl.WriteOnly, 4)
+	defer writeBuff.Free()
+
+	readBuff := world.Malloc(xcl.readOnly, 4)
+	defer readBuff.Free()
+
 
 	krnl.SetArg(0, 1)
 	krnl.SetArg(1, 2)
-	krnl.SetMemoryArg(2, buff)
+	krnl.SetMemoryArg(readBuff, writeBuff)
 
 	krnl.Run(1, 1, 1)
 
 	resp := make([]byte, 4)
-	buff.Read(resp)
+	writeBuff.Read(resp)
 
 	var ret uint32
 	err := binary.Read(bytes.NewReader(resp), binary.LittleEndian, &ret)
