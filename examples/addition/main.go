7ffbd135c1c51d73ad5d4bebe21a66cd49286d7c
Josh Bohde <josh@joshbohde.com>
2017-02-07 15:48:42 -0600

    start to rewrite parameters.

--- examples/addition/main.go
+++ examples/addition/main.go
@@ -4,43 +4,26 @@ import (
 	// import the entire framework (including bundled verilog)
 	_ "sdaccel"
 
-	"sdaccel/control"
 	"sdaccel/memory"
 )
 
 // magic identifier for exporting
 func Top(
-	controlAddr chan<- uint32,
-	controlData <-chan uint32,
+	a uint32,
+	b uint32,
+	addr uintptr,
 
 	memReadAddr chan<- memory.Addr,
 	memReadData <-chan memory.ReadData,
 
 	memWriteAddr chan<- memory.Addr,
 	memWriteData chan<- memory.WriteData,
-	memResp <-chan memory.Response,
-
-	controlReadAddr <-chan control.Addr,
-	controlReadData chan<- control.ReadData,
-
-	controlWriteAddr <-chan control.Addr,
-	controlWriteData <-chan control.WriteData,
-	controlResp chan<- control.Resp) {
-
-	// Disable AXI-Lite control accesses.
-	go control.DisableReads(controlReadAddr, controlReadData)
-	go control.DisableWrites(controlWriteAddr, controlWriteData, controlResp)
+	memResp <-chan memory.Response) {
 
 	// Disable memory reads
 	go memory.DisableReads(memReadAddr, memReadData)
 
-	read := func(a uint32) uint32 {
-		controlAddr <- a
-		return <-controlData
-	}
-
-	val := read(0x40) + read(0x44)
-	addr := uint64(read(0x4C))<<32 | uint64(read(0x48))
+	val := a + b
 
 	memory.Write(addr, val, memWriteAddr, memWriteData, memResp)
 }
