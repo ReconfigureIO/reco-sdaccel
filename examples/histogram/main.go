2f994a39e901f80355103a49fb581519ea40e861
Josh Bohde <josh@joshbohde.com>
2017-02-09 12:28:18 -0600

    update to new format.

--- examples/histogram/main.go
+++ examples/histogram/main.go
@@ -3,51 +3,23 @@ package main
 import (
 	// import the entire framework (including bundled verilog)
 	_ "sdaccel"
-	"sdaccel/control"
 	"sdaccel/memory"
-)
-
-const (
-	INPUT_INDEX  = 0x40
-	OUTPUT_INDEX = 0x48
-	LENGTH_INDEX = 0x48 + 8
 )
 
 type Histogram [512]uint32
 
 // magic identifier for exporting
 func Top(
-	controlAddr chan<- uint32,
-	controlData <-chan uint32,
+	inputData uintptr,
+	outputData uintptr,
+	length uint32,
 
 	memReadAddr chan<- memory.Addr,
 	memReadData <-chan memory.ReadData,
 
 	memWriteAddr chan<- memory.Addr,
 	memWriteData chan<- memory.WriteData,
-	memResp <-chan memory.Response,
-
-	controlReadAddr <-chan control.Addr,
-	controlReadData chan<- control.ReadData,
-
-	controlWriteAddr <-chan control.Addr,
-	controlWriteData <-chan control.WriteData,
-	controlResp chan<- control.Resp) {
-
-	readParam := func(a uint32) uint32 {
-		controlAddr <- a
-		return <-controlData
-	}
-
-	readAddressParam := func(a uint32) uint64 {
-		return (uint64(readParam(a+4)) << 32) + uint64(readParam(a))
-	}
-
-	inputData := readAddressParam(INPUT_INDEX)
-	outputData := readAddressParam(OUTPUT_INDEX)
-
-	// how many samples are there? (uint32)
-	length := readParam(LENGTH_INDEX)
+	memResp <-chan memory.Response) {
 
 	var histogram Histogram
 
@@ -60,6 +32,7 @@ func Top(
 
 	endAddr := outputData + (512 * 4)
 	for i := 0; i < 512; i++ {
-		memory.Write(outputData, histogram[i], memWriteAddr, memWriteData, memResp)
+		memory.Write(endAddr, histogram[i], memWriteAddr, memWriteData, memResp)
+		endAddr += 4
 	}
 }
