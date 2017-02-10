fead907c86b1cd735ae797df5223edc714dfb21e
Josh Bohde <josh@joshbohde.com>
2017-02-10 08:53:40 -0600

    fix up hostside program.

--- examples/histogram/cmd/test-histogram/main.go
+++ examples/histogram/cmd/test-histogram/main.go
@@ -18,7 +18,8 @@ func main() {
 	buff := world.Malloc(xcl.ReadOnly, 4*2)
 	defer buff.Free()
 
-	outputBuff := world.Malloc(xcl.ReadOnly, 4*512)
+	resp := make([]byte, 4*512)
+	outputBuff := world.Malloc(xcl.ReadWrite, uint(len(resp)))
 	defer outputBuff.Free()
 
 	input := []uint32{10, 2 ^ 31}
@@ -27,13 +28,14 @@ func main() {
 	binary.Write(inputBuff, binary.LittleEndian, &input)
 	buff.Write(inputBuff.Bytes())
 
+	outputBuff.Write(resp)
+
 	krnl.SetMemoryArg(0, buff)
 	krnl.SetMemoryArg(1, outputBuff)
 	krnl.SetArg(2, 2)
 
 	krnl.Run(1, 1, 1)
 
-	resp := make([]byte, 4*512)
 	outputBuff.Read(resp)
 
 	var ret [512]uint32
@@ -44,12 +46,22 @@ func main() {
 
 	sum := uint32(0)
 
-	for _, val := range ret {
+	for i, val := range ret {
 		sum += val
 	}
 
-	fmt.Printf("found %d samples\n", sum)
 	if sum != 2 {
+		fmt.Printf("expected %d samples, found %d\n", 2, sum)
+		os.Exit(1)
+	}
+
+	if ret[0] != 1 {
+		fmt.Printf("ret[0] != %d\n", 1)
+		os.Exit(1)
+	}
+
+	if ret[256] != 1 {
+		fmt.Printf("ret[256] != %d\n", 1)
 		os.Exit(1)
 	}
 
