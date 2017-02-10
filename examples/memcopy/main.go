4cbe89670ad382f7fb924499706d1efdd642c5d9
Max Siegieda <maxsiegieda@gmail.com>
2017-02-10 14:13:02 +0000

    change bulk of memcopy to use new memory API

--- examples/memcopy/main.go
+++ examples/memcopy/main.go
@@ -3,62 +3,30 @@ package main
 import (
 	// import the entire framework (including bundled verilog)
 	_ "sdaccel"
-	"sdaccel/control"
 	"sdaccel/memory"
-)
-
-const (
-	INPUT_INDEX  = 0x40
-	OUTPUT_INDEX = 0x48
-	LENGTH_INDEX = 0x48 + 8
 )
 
 // magic identifier for exporting
 func Top(
-	controlAddr chan<- uint32,
-	controlData <-chan uint32,
+	inputData uintptr,
+	outputData uintptr,
+	length uint32,
 
-	// memReadAddr chan<- memory.Addr,
-	// memReadData <-chan memory.ReadData,
+	memReadAddr chan<- memory.Addr,
+	memReadData <-chan memory.ReadData,
 
-	// memWriteAddr chan<- memory.Addr,
-	// memWriteData chan<- memory.WriteData,
-	// memResp <-chan memory.Response,
-
-	controlReadAddr <-chan control.Addr,
-	controlReadData chan<- control.ReadData,
-
-	controlWriteAddr <-chan control.Addr,
-	controlWriteData <-chan control.WriteData,
-	controlResp chan<- control.Resp) {
-
-	readParam := func(a uint32) uint32 {
-		controlAddr <- a
-		return <-controlData
-	}
-
-	readAddressParam := func(a uint32) uint64 {
-		return (uint64(readParam(a+4)) << 32) + uint64(readParam(a))
-	}
-
-	inputData := readAddressParam(INPUT_INDEX)
-	outputData := readAddressParam(OUTPUT_INDEX)
+	memWriteAddr chan<- memory.Addr,
+	memWriteData chan<- memory.WriteData,
+	memResp <-chan memory.Response) {
 
 	// how many samples are there? (uint32)
 	length := readParam(LENGTH_INDEX)
 
 	for ; length > 0; length-- {
-		sample := memory.Read(inputData, controlReadAddr, controlReadData)
-		memory.Write(outputData, sample, controlWriteAddr, controlWriteData, controlResp)
+		sample := memory.Read(inputData, memReadAddr, memReadData)
+		memory.Write(outputData, sample, memWriteAddr, memWriteData, memResp)
 		inputData += 4
 
 	}
 
 }
-
-
-// func Top(
-// 	inputData uintptr,
-// 	outputData uintptr,
-// 	length uint32,
-//
