27f174d60bbd457d6893cf310fb25907bc16aefe
Josh Bohde <josh@joshbohde.com>
2017-02-13 12:51:53 -0600

    update kernel name.

--- examples/histogram-parallel/cmd/test-histogram-parallel/main.go
+++ examples/histogram-parallel/cmd/test-histogram-parallel/main.go
@@ -9,7 +9,7 @@ func main() {
 	world := xcl.NewWorld()
 	defer world.Release()
 
-	krnl := world.Import("kernel_test").GetKernel("reconfigure_io_reco_sdaccel_stub_0_1")
+	krnl := world.Import("kernel_test").GetKernel("reconfigure_io_sdaccel_builder_stub_0_1")
 	defer krnl.Release()
 
 	buff := world.Malloc(xcl.ReadOnly, 2)
