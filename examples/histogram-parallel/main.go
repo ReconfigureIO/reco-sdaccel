de2d953812cd1bb77aaa056030a5492159675968
Max Siegieda <maxsiegieda@gmail.com>
2017-02-21 10:29:26 +0000

    split memory updating into a separate 'apply' function

--- examples/histogram-parallel/main.go
+++ examples/histogram-parallel/main.go
@@ -19,21 +19,26 @@ func Top(
 	memWriteData chan<- memory.WriteData,
 	memResp <-chan memory.Response) {
 
+	pointerDiff := make([]uint32, 8)
+
 	// The host needs to provide the length we should read
 	for ; length > 0; length-- {
 		// First we'll read each sample
 		sample := memory.Read(inputData, memReadAddr, memReadData)
 		// If we think of external memory we are writing to as a [512]uint32, this would be the index we access
 		index := uint16(sample) >> (16 - 9)
-		pointerDiff := index << 2
+		pointerDiff[length] := index << 2
 		// And this is that index as a pointer to external memory
-		outputPointer := outputData + uintptr(pointerDiff)
-
-		current := memory.Read(outputPointer, memReadAddr, memReadData)
-
-		memory.Write(outputPointer, current+1, memWriteAddr, memWriteData, memResp)
-
-		inputData += 4
 	}
 }
+	
+func apply(bins []uint32) {
+	
+	outputPointer := outputData + uintptr(bins)
 
+	current := memory.Read(outputPointer, memReadAddr, memReadData)
+
+	memory.Write(outputPointer, current+1, memWriteAddr, memWriteData, memResp)
+
+	inputData += 4
+}
