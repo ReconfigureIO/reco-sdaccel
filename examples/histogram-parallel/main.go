2e6ac82bf0871389f354f60ce49edecb1543e8a8
Max Siegieda <maxsiegieda@gmail.com>
2017-02-28 11:50:58 +0000

    FPGA code should look more like this

--- examples/histogram-parallel/main.go
+++ examples/histogram-parallel/main.go
@@ -6,25 +6,6 @@ import (
 	"sdaccel/memory"
 )
 
-type MemoryOpParams struct {
-	op int8
-	memOffset uintptr
-	data uint32
-}
-
-func MemoryOp(p MemoryOpParams) uint32 {
-
-	if p.op==0 {
-		return memory.Read(p.memOffset, memReadAddr, memReadData)
-	}
-	else if p.op==1 { //write
-		memory.Write(p.memOffset, p.data, memWriteData, memWriteData)
-	}
-	else if p.op==2 { //write to response-to-host channel
-		memory.Write(p.memOffset, p.data, memWriteData, memWriteData, memResp)
-	}
-}
-
 // magic identifier for exporting
 func Top(
 	inputData uintptr,
@@ -38,25 +19,49 @@ func Top(
 	memWriteData chan<- memory.WriteData,
 	memResp <-chan memory.Response) {
 
-	samples := make([]uint32, 8)
-	binAddrs := make([]uintptr, 8)
+	//set up worker input and output channels, create worker for each pair
+	var workerinput [1]chan int
+	var workeroutput [1]chan int
+	for i := range workerinput {
+   		workerinput[i] = make(chan uint32)
+   		workeroutput[i] = make(chan uint32)
+		Worker(workerinput[i], workeroutput[i])
+	}
+	//trigger function to update bin values based on worker output
+	memUpdate()
 
 	// The host needs to provide the length we should read
 	for ; length > 0; length-- {
 		// First we'll read each sample
-		samples[length] := MemoryOp(MemoryOpParams{op: 0, memOffset: inputData})
+		sample := memory.Read(inputData, memReadAddr, memReadData)
+		//length modulo number of workers determines which worker to give sample to
+		workerNo = length % 1
+		workerinput[workerNo]<- sample
 	}
 
-	for i := 0; i < samples.length; i++ {
+
+func worker(chan input, chan output) {
+
+	for {
+		sample := <-input
 		//cut sample down to 9 MSBs to determine destination bin
-		index := uint16(sample) >> (16 - 9)
-		binAddrs[] := index << 2
-		// And this is that index as a pointer to external memory
-	}
-
-	for i := 0; i < binAddrs.length; i++ {
-		current := MemoryOp(MemoryOpParams{op: 0, memOffset: binAddrs[i]})
-		MemoryOp(MemoryOpParams{op: 2, memOffset: binAddrs[i], data: current+1})
+		binNo := uint16(sample) >> (16 - 9)
+		output<- binNo
 	}
 
 }
+
+
+	func memUpdate() {
+
+		for {
+			binNo := <-workeroutput[current]
+			binPtr := binNo << 2
+			current := memory.Read(binPtr, memReadAddr, memReadData)
+			memory.Write(binPtr, current+1, memWriteData, memWriteData, memResp)
+			current := current+1
+		}
+
+
+}
+
