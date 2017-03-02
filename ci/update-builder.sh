e5658234639eb92b8edd57642e37b67f9e2bb1cf
Josh Bohde <josh@joshbohde.com>
2017-03-02 15:49:09 -0600

    Add script to update wrapper version and update to desired wrapper.

---
+++ ci/update-builder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+VERSION="$1"
+BRANCH=bump/update-sdaccel-to-$VERSION
+git checkout -b "$BRANCH"
+sed -i "s/^SDACCEL_WRAPPER_VERSION\ :=.*/SDACCEL_WRAPPER_VERSION\ :=\ ${VERSION}/g" Makefile
+sed -i "s/defaultValue:\ 'v*.*.*'/defaultValue:\ '${VERSION}'/g" Jenkinsfile
+git add Makefile
+git commit -m "[auto] update sdaccel wrapper to ${VERSION}"
+git push -u origin "$BRANCH"
+hub pull-request -m "[auto] bump donut to $VERSION"
