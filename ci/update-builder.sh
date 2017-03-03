33c1e62824cf15ae1272a991df05da2bf3de27b3
Josh Bohde <josh@joshbohde.com>
2017-03-03 15:56:21 -0600

    fix update script.

--- ci/update-builder.sh
+++ ci/update-builder.sh
@@ -5,7 +5,7 @@ BRANCH=bump/update-sdaccel-to-$VERSION
 git checkout -b "$BRANCH"
 sed -i "s/^SDACCEL_WRAPPER_VERSION\ :=.*/SDACCEL_WRAPPER_VERSION\ :=\ ${VERSION}/g" Makefile
 sed -i "s/defaultValue:\ 'v*.*.*'/defaultValue:\ '${VERSION}'/g" Jenkinsfile
-git add Makefile
+git add Makefile Jenkinsfile
 git commit -m "[auto] update sdaccel wrapper to ${VERSION}"
 git push -u origin "$BRANCH"
 hub pull-request -m "[auto] bump donut to $VERSION"
