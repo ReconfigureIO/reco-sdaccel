f26daa98d95fa0b219bf2e1fc72cfdd90e93cf3b
Josh Bohde <josh@joshbohde.com>
2017-03-15 15:35:21 -0500

    Add comments.

--- examples/histogram-parallel/main.go
+++ examples/histogram-parallel/main.go
@@ -25,14 +25,19 @@ func Top(
 	incrChan := make(chan uintptr)
 	incrResp := make(chan uint32)
 
-	// handle memory reading & writing
 	go func() {
 		for {
 			var addr uintptr
 
+			// This block provides single access to memory, with two
+			// separate operations: reading or incrementing a pointer
+			// in memory.
+
+			// All access is through channels allowing operations to
+			// be enqueued, only blocking when the response is needed
 			select {
 			case addr = <-readChan:
-				readRespChan := memory.Read(addr, memReadAddr, memReadData)
+				readRespChan <- memory.Read(addr, memReadAddr, memReadData)
 			case addr = <-incrChan:
 				current := memory.Read(addr, memReadAddr, memReadData)
 				memory.Write(addr, current+1, memWriteAddr, memWriteData, memResp)
@@ -41,28 +46,33 @@ func Top(
 		}
 	}()
 
-	// queue up all read operations
 	go func() {
+		// length is the number of addresses we are supposed to read
+		// so this block enqueues each address to read.
 		for i := length; i > 0; i-- {
 			readChan <- inputData
 			inputData += 4
 		}
 	}()
 
-	// for every read response, queue up the increment operations
 	go func() {
 		for i := length; i > 0; i-- {
+			// get the read response that was previously enqueued.
 			sample := <-readRespChan
 			// If we think of external memory we are writing to as a [512]uint32, this would be the index we access
 			index := uint16(sample) >> (16 - 9)
 			pointerDiff := index << 2
 			// And this is that index as a pointer to external memory
-			incrChan <- outputData + uintptr(pointerDiff)
+			outputPointer := outputData + uintptr(pointerDiff)
+			// enqueue an increment operation on that pointer
+			incrChan <- outputPointer
 		}
 	}()
 
-	// drain all the increment operations
+	// Wait for each response for increment operations
 	for i := length; i > 0; i-- {
 		<-incrResp
 	}
+
+	// Once that's done, we can exit.
 }
