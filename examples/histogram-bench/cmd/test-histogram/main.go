5412f83a97fd4e22e6e5fad917832416b98e6116
Patrick Thomson <patrick.thomson@reconfigure.io>
2017-03-24 11:56:04 -0400

    this doesn't really work

---
+++ examples/histogram-bench/cmd/test-histogram/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math/rand"
+	"reflect"
+	"xcl"
+	"testing"
+)
+
+const (
+	MAX_BIT_WIDTH       = 16
+	HISTOGRAM_BIT_WIDTH = 9
+	HISTOGRAM_WIDTH     = 1 << 9
+)
+
+func main() {
+	bm := testing.Benchmark(func(B *testing.B)  {
+		B.Run("histo", doit)
+	})
+
+	println(bm.String())
+}
+
+func doit(B *testing.B) {
+	world := xcl.NewWorld()
+	defer world.Release()
+
+	krnl := world.Import("kernel_test").GetKernel("reconfigure_io_sdaccel_builder_stub_0_1")
+	defer krnl.Release()
+
+	input := make([]uint32, 20)
+
+	// seed it with 20 random values, bound to 0 - 2**16
+	for i, _ := range input {
+		input[i] = uint32(uint16(rand.Uint32()))
+	}
+
+	buff := world.Malloc(xcl.ReadOnly, uint(binary.Size(input)))
+	defer buff.Free()
+
+	resp := make([]byte, 4*HISTOGRAM_WIDTH)
+	outputBuff := world.Malloc(xcl.ReadWrite, uint(binary.Size(resp)))
+	defer outputBuff.Free()
+
+	binary.Write(buff.Writer(), binary.LittleEndian, &input)
+
+	outputBuff.Write(resp)
+
+	krnl.SetMemoryArg(0, buff)
+	krnl.SetMemoryArg(1, outputBuff)
+	krnl.SetArg(2, uint32(len(input)))
+
+	krnl.Run(1, 1, 1)
+
+	var ret [512]uint32
+	err := binary.Read(outputBuff.Reader(), binary.LittleEndian, &ret)
+	if err != nil {
+		log.Fatal("binary.Read failed:", err)
+	}
+
+	var expected [HISTOGRAM_WIDTH]uint32
+
+	for _, val := range input {
+		expected[val>>(MAX_BIT_WIDTH-HISTOGRAM_BIT_WIDTH)] += 1
+	}
+
+	if !reflect.DeepEqual(expected, ret) {
+		log.Fatalf("%v != %v\n", ret, expected)
+	}
+
+	for i, val := range ret {
+		fmt.Printf("%d: %d\n", i<<(MAX_BIT_WIDTH-HISTOGRAM_BIT_WIDTH), val)
+	}
+
+}
