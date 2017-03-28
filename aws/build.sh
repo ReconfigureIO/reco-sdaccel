0519c55ba64e45965550e61e9b3fe16d1c7259fc
Josh Bohde <josh@joshbohde.com>
2017-03-28 18:11:09 -0500

    Update for sdaccel.

--- aws/build.sh
+++ aws/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-export PATH=$XILINX_VIVADO/bin:$PATH
+export PATH=$XILINX_SDX/bin:$XILINX_VIVADO/bin:$PATH
 
 aws s3 cp "$INPUT_URL" - | tar zxf -
 /opt/sdaccel-builder/sdaccel-builder build
