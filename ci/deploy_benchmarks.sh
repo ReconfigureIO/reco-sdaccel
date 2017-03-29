ea16d2229af6f25672aeaf98f0edf30dbf555fbe
Josh Bohde <josh@joshbohde.com>
2017-03-29 13:30:56 -0500

    Store logs on s3 as well in case of loss of Jenkins.

--- ci/deploy_benchmarks.sh
+++ ci/deploy_benchmarks.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 mkdir -p benchmarks/repository
+# Upload local logs
+aws s3 sync benchmarks/logs s3://nerabus/reco-sdaccel/benchmarks/
+# Then download them all
+aws s3 sync s3://nerabus/reco-sdaccel/benchmarks/ benchmarks/logs
+# Generate site
 docker run -v "$PWD":/mnt -w /mnt/benchmarks "parfunc/gipeda:latest" gipeda
+# Upload generated website
 aws s3 sync benchmarks/site s3://perf.reconfigureio-infra.com/reco-sdaccel/
