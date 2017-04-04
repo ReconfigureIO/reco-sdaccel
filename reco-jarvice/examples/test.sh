5967fbd74d10a767dc045849bd83e0dffa6341ed
Patrick Thomson <patrick.thomson@reconfigure.io>
2017-04-04 16:12:35 -0400

    fix XILINX_SDACCEL version

--- reco-jarvice/examples/test.sh
+++ reco-jarvice/examples/test.sh
@@ -7,7 +7,7 @@ git clone https://github.com/Xilinx/SDAccel_Examples.git examples
 
 cd examples/getting_started/basic/hello
 
-source /opt/Xilinx/SDx/2016.3/settings64.sh
+source /opt/Xilinx/SDx/2016.4/settings64.sh
 export LC_ALL=C
 export LM_LICENSE_FILE=2100@l3976.local.nimbix.net
 
--- workflows/build.sh
+++ workflows/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 export LC_ALL=C
-export XILINX_SDACCEL=/opt/Xilinx/SDx/2016.3
+export XILINX_SDACCEL=/opt/Xilinx/SDx/2016.4
 export XILINX_SDX="$XILINX_SDACCEL"
 export XILINX_VIVADO="$XILINX_SDACCEL/Vivado"
 export XILINXD_LICENSE_FILE=2100@l3976.local.nimbix.net
