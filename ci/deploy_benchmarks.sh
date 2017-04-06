25e0f9ebe058e9873333435f301032496781e965
Patrick Thomson <patrick.thomson@reconfigure.io>
2017-04-06 11:58:05 -0400

    and remove old benchmarks

--- ci/deploy_benchmarks.sh
+++ ci/deploy_benchmarks.sh
@@ -15,3 +15,5 @@ aws s3 sync s3://nerabus/reco-sdaccel/benchmarks/ benchmarks/logs
 docker run -v "$PWD":/mnt -w /mnt/benchmarks "parfunc/gipeda:latest" gipeda
 # Upload generated website
 aws s3 sync benchmarks/site s3://perf.reconfigureio-infra.com/reco-sdaccel/
+
+rm bench_tmp/*
