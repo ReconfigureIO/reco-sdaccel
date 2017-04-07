28e1b879f257a7c51312d21426d3d9820c348db0
Patrick Thomson <patrick.william.thomson@gmail.com>
2017-04-07 16:24:53 -0400

    Separate deploy/upload of benchmarks into two stages (#86)
    * Separate deploy/upload of benchmarks into two stages
    
    adds upload_benchmarks, invokes it, then invokes the job
    
    * extra mkdir

--- ci/deploy_benchmarks.sh
+++ ci/deploy_benchmarks.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 set -e
 
-SHA=$(git rev-parse HEAD)
 mkdir -p benchmarks/repository benchmarks/logs
 
-# Join intermediate log files
-cat bench_tmp/* > benchmarks/logs/"$SHA".log
-
-# Upload local logs
-aws s3 sync benchmarks/logs s3://nerabus/reco-sdaccel/benchmarks/
-# Then download them all
+# Download them all
 aws s3 sync s3://nerabus/reco-sdaccel/benchmarks/ benchmarks/logs
 # Generate site
 docker run -v "$PWD":/mnt -w /mnt/benchmarks "parfunc/gipeda:latest" gipeda
