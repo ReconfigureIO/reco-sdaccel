23fee0dad1b33512c5e2f0ec2d3b6b113023ad2c
Josh Bohde <josh@joshbohde.com>
2017-04-13 09:06:28 -0500

    POST callback with the expectation that it's an event url.

--- aws/simulate.sh
+++ aws/simulate.sh
@@ -2,13 +2,13 @@
 set -e
 export PATH=$XILINX_SDX/bin:$XILINX_VIVADO/bin:$PATH
 source "/opt/sdaccel-builder/settings.sh"
-#curl -XPATCH -H "Content-Type: application/json"  -d '{"status": "PROCESSING"}' "$CALLBACK_URL" > /dev/null
+curl -XPOST -H "Content-Type: application/json"  -d '{"status": "STARTED"}' "$CALLBACK_URL" > /dev/null
 
 set +e
 aws s3 cp "$INPUT_URL" - | tar zxf -
 if [ $? -ne 0 ]; then
     exit="$?"
-    curl -XPATCH -H "Content-Type: application/json"  -d '{"status": "ERROR"}' "$CALLBACK_URL" > /dev/null
+    curl -XPOST -H "Content-Type: application/json"  -d '{"status": "ERRORED"}' "$CALLBACK_URL" > /dev/null
     exit "$exit"
 fi
 
@@ -16,8 +16,8 @@ fi
 
 if [ $? -ne 0 ]; then
     exit="$?"
-    curl -XPATCH -H "Content-Type: application/json"  -d '{"status": "ERROR"}' "$CALLBACK_URL" > /dev/null
+    curl -XPOST -H "Content-Type: application/json"  -d '{"status": "ERRORED"}' "$CALLBACK_URL" > /dev/null
     exit "$exit"
 fi
 
-curl -XPATCH -H "Content-Type: application/json"  -d '{"status": "COMPLETED"}' "$CALLBACK_URL" > /dev/null
+curl -XPOST -H "Content-Type: application/json"  -d '{"status": "COMPLETED"}' "$CALLBACK_URL" > /dev/null
