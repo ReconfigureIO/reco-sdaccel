8da923b6d893b998b624f44c3a46cff012baa111
Josh Bohde <josh@joshbohde.com>
2017-06-23 15:49:34 -0500

    use reco-aws for build process.

--- ci/test_build.sh
+++ ci/test_build.sh
@@ -10,12 +10,12 @@ SHA=$4
 ERRFILE=$(mktemp --suffix ".log")
 
 cd "examples/$NAME"
-NUMBER=$(../../reco-jarvice/reco-jarvice build 2>$ERRFILE)
+NUMBER=$(../../reco-aws/reco-aws build 2>$ERRFILE)
 cat $ERRFILE
 
 mkdir -p ../../bench_tmp
 TMPFILE=$(mktemp --suffix ".log" -p ../../bench_tmp)
 cat $ERRFILE | grep "verilog," -A2 | awk -F"," "{print \"buildTime/\" \$1 \"/$NAME\" \";\" \$2}" | tee -a "$TMPFILE"
 rm $ERRFILE
-../../reco-jarvice/reco-jarvice run "$NUMBER" "$EXAMPLE"
-../../reco-jarvice/reco-jarvice run "$NUMBER" "bench-$BENCH" 2>&1 | tee -a "$TMPFILE"
+../../reco-aws/reco-aws run "$NUMBER" "$EXAMPLE"
+../../reco-aws/reco-aws run "$NUMBER" "bench-$BENCH" 2>&1 | tee -a "$TMPFILE"
