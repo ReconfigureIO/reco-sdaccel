7d7ad6351df2361d3db5dfa250f62116c6ab9ef1
Josh Bohde <josh@joshbohde.com>
2017-06-28 14:51:45 -0500

    Cleanup output.

--- aws/simulate.sh
+++ aws/simulate.sh
@@ -2,13 +2,13 @@
 set -e
 export PATH=$XILINX_SDX/bin:$XILINX_VIVADO/bin:$PATH
 source "/opt/sdaccel-builder/settings.sh"
-curl -XPOST -H "Content-Type: application/json"  -d '{"status": "STARTED"}' "$CALLBACK_URL" > /dev/null
+curl -XPOST -H "Content-Type: application/json"  -d '{"status": "STARTED"}' "$CALLBACK_URL" &> /dev/null
 
 set +e
 aws s3 cp --quiet "$INPUT_URL" - | tar zxf -
 if [ $? -ne 0 ]; then
     exit="$?"
-    curl -XPOST -H "Content-Type: application/json"  -d '{"status": "ERRORED"}' "$CALLBACK_URL" > /dev/null
+    curl -XPOST -H "Content-Type: application/json"  -d '{"status": "ERRORED"}' "$CALLBACK_URL" &> /dev/null
     exit "$exit"
 fi
 
@@ -17,8 +17,8 @@ fi
 exit="$?"
 
 if [ $exit -ne 0 ]; then
-    curl -XPOST -H "Content-Type: application/json"  -d '{"status": "ERRORED"}' "$CALLBACK_URL" > /dev/null
+    curl -XPOST -H "Content-Type: application/json"  -d '{"status": "ERRORED"}' "$CALLBACK_URL" &> /dev/null
     exit "$exit"
 fi
 
-curl -XPOST -H "Content-Type: application/json"  -d '{"status": "COMPLETED"}' "$CALLBACK_URL" > /dev/null
+curl -XPOST -H "Content-Type: application/json"  -d '{"status": "COMPLETED"}' "$CALLBACK_URL" &> /dev/null
