4d44ed37753988c1d25af733ff93b30447de86af
Josh Bohde <josh@joshbohde.com>
2017-07-17 14:43:24 -0500

    Publish latest release as well.

--- ci/release.sh
+++ ci/release.sh
@@ -4,6 +4,7 @@ VERSION="$1"
 git tag "$VERSION"
 git push --tags
 make clean release
+make VERSION=latest upload-reco-check-bundle
 make update-changelog
 git add -u
 git commit -m "[auto] finalize release $VERSION"
