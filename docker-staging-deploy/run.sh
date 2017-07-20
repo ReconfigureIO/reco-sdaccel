39899628e97b39394396710613b6c9e0adbd1273
Josh Bohde <josh@joshbohde.com>
2017-07-20 10:35:44 -0500

    set it to HW mode.

--- docker-staging-deploy/run.sh
+++ docker-staging-deploy/run.sh
@@ -4,5 +4,5 @@ unzip /tmp/bundle.zip -d "$PWD"
 
 source "$XILINX_SDX"/settings64.sh
 export PATH=$PWD/.reco-work/sdaccel/dist/:$PATH
-export XCL_EMULATION_MODE=hw_emu XCL_BINDIR="$PWD/.reco-work/sdaccel/dist/xclbin"
+export XCL_EMULATION_MODE=hw XCL_BINDIR="$PWD/.reco-work/sdaccel/dist/xclbin"
 bash -c "$CMD"
