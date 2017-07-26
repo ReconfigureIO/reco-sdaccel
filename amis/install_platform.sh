0593d028d9c69a31e794aaa123c8ec6de4eb1fcb
Josh Bohde <josh@joshbohde.com>
2017-07-26 16:53:58 -0500

    Update to install the runtime env.

--- amis/install_platform.sh
+++ amis/install_platform.sh
@@ -10,9 +10,13 @@ export XILINX_SDX=/opt/Xilinx/SDx/2017.1.op/
 export AWS_FPGA_REPO_DIR="$PWD"
 export SDK_DIR="$PWD/sdk"
 
-# workaround their usage of module
-module() { true; }
-
-source sdk/SDAccel/sdaccel_setup.sh -d
+source $XILINX_SDX/settings.sh
+cd sdk
+source sdk_install.sh
+cd SDAccel
+# This isn't correctly generated in sdaccel_setup.sh
+make ec2=1 -C userspace/src
+source sdaccel_setup.sh
+cd ../..
 cp -R ./sdk/SDAccel/aws_platform/xilinx_aws-vu9p-f1_4ddr-xpr-2pr_4_0/ "$XILINX_SDX/platforms"
 rm -rf /tmp/aws-fpga
--- docker-staging-deploy/run.sh
+++ docker-staging-deploy/run.sh
@@ -2,7 +2,7 @@
 aws s3 cp "$DIST_URL" /tmp/bundle.zip
 unzip /tmp/bundle.zip -d "$PWD"
 
-source "$XILINX_SDX"/settings64.sh
+source "${XILINX_SDX%.op}.rte/setup.sh"
 export PATH=$PWD/.reco-work/sdaccel/dist/:$PATH
 export XCL_EMULATION_MODE=hw XCL_BINDIR="$PWD/.reco-work/sdaccel/dist/xclbin"
 bash -c "$CMD"
