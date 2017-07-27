d307c2d4b0ba8c1249b254947cce7b761b5e4e82
Josh Bohde <josh@joshbohde.com>
2017-07-27 13:39:44 -0500

    load image.

--- docker-staging-deploy/run.sh
+++ docker-staging-deploy/run.sh
@@ -2,6 +2,10 @@
 aws s3 cp "$DIST_URL" /tmp/bundle.zip
 unzip /tmp/bundle.zip -d "$PWD"
 
+fpga-describe-local-image -S 0 -H –R
+fpga-load-local-image -S 0 -I "$AGFI"
+fpga-describe-local-image -S 0 -H –R
+
 source "${XILINX_SDX%.op}.rte/setup.sh"
 export PATH=$PWD/.reco-work/sdaccel/dist/:$PATH
 export XCL_EMULATION_MODE=hw XCL_BINDIR="$PWD/.reco-work/sdaccel/dist/xclbin"
