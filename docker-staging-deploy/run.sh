cdd7ca17865d009cea6b34ed0c8192cd08402d69
Josh Bohde <josh@joshbohde.com>
2017-07-31 14:51:53 -0500

    wait until the fpga has loaded before running.

--- docker-staging-deploy/run.sh
+++ docker-staging-deploy/run.sh
@@ -5,9 +5,15 @@ unzip /tmp/bundle.zip -d "$PWD"
 
 fpga-describe-local-image -S 0 -H –R
 fpga-load-local-image -S 0 -I "$AGFI"
+# Wait until the FPGA has loaded
+until [ "$(fpga-describe-local-image -S 0 | head -n1 | tr -s ' ' | cut -d' ' -f4)" = "loaded" ]; do
+    sleep 1s
+done
 fpga-describe-local-image -S 0 -H –R
 
+source "${XILINX_SDX}/settings64.sh"
 source "${XILINX_SDX%.op}.rte/setup.sh"
 export PATH=$PWD/.reco-work/sdaccel/dist/:$PATH
-export XCL_EMULATION_MODE=hw XCL_BINDIR="$PWD/.reco-work/sdaccel/dist/xclbin"
+export XCL_BINDIR="$PWD/.reco-work/sdaccel/dist/xclbin"
+cd "$PWD/.reco-work/sdaccel/dist"
 bash -c "$CMD"
