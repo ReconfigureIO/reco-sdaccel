5bbe78e4da18b690c74b691589a2a4b3056a6dda
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-08-08 14:43:20 -0500

    updated regression example

--- examples/regression/cmd/test-regression/main.go
+++ examples/regression/cmd/test-regression/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/binary"
+	"math/rand"
 	"fmt"
 	//"os"
 	"xcl"
@@ -30,6 +31,18 @@ func main() {
 	krnl := world.Import("kernel_test").GetKernel("reconfigure_io_sdaccel_builder_stub_0_1")
 	defer krnl.Release()
 
+	// The data we'll send to the kernel for processing
+        input := [8]uint32{2,3,5,7,11,13,17,19}
+
+	// seed it with 20 random values, bound to 0 - 2**16
+	for i, _ := range input {
+		input[i] = uint32(uint16(rand.Uint32()))
+	}
+
+	// On the FGPA, allocated ReadOnly memory for the input to the kernel.
+	inputBuff := world.Malloc(xcl.ReadOnly, uint(binary.Size(input)))
+	defer inputBuff.Free()
+
 	// Allocate a buffer on the FPGA to store the return value of our computation
 	// The output is a uint32, so we need 4 bytes to store it
 	buff1 := world.Malloc(xcl.WriteOnly, 4)
@@ -40,21 +53,24 @@ func main() {
 
 	// Pass the arguments to the kernel
 	// Set the pointer to the first output buffer
-	krnl.SetArg(0, 1)
-	krnl.SetMemoryArg(1, buff1)
-	krnl.SetMemoryArg(2, buff2)
-        //krnl.SetMemoryArg(1, buff2)
+	krnl.SetMemoryArg(0, inputBuff)
+        krnl.SetArg(1, 8)
+	krnl.SetMemoryArg(2, buff1)
+	krnl.SetMemoryArg(3, buff2)
+
+	// write our input to the kernel at the memory we've previously allocated
+	binary.Write(inputBuff.Writer(), binary.LittleEndian, &input)
 
 	// Run the kernel with the supplied arguments
 	krnl.Run(1, 1, 1)
 
-	// Decode that byte slice into the uint32 we're expecting
-	var alpha uint32
+	// Decode that byte slice into the int32 we're expecting
+	var alpha int32
 	err1 := binary.Read(buff1.Reader(), binary.LittleEndian, &alpha)
 	if err1 != nil {
 		fmt.Println("binary.Read failed:", err1)
 	}
-	var beta uint32
+	var beta int32
 	err2 := binary.Read(buff2.Reader(), binary.LittleEndian, &beta)
 	if err2 != nil {
 		fmt.Println("binary.Read failed:", err2)
@@ -67,6 +83,6 @@ func main() {
         beta_scale = float32(beta)/1024
 
 	// Print the value we got from the FPGA
-	fmt.Printf("\n%f\n%f\n", alpha_scale, beta_scale)
+	fmt.Printf("%f\n%f\n", alpha_scale, beta_scale)
 
 }
--- examples/regression/main.go
+++ examples/regression/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	// Import the entire framework (including bundled verilog)
 	_ "sdaccel"
 
+	axiarbitrate "axi/arbitrate"
 	aximemory "axi/memory"
 	axiprotocol "axi/protocol"
 )
@@ -20,9 +20,10 @@ func Top(
 	// For this example, we have 3 arguments: two operands to add
 	// together and an address to memory (the uuint32s) on the FPGA to
 	// store the output (the uintptr)
-	a uint32,
-	addr1 uintptr,
-        addr2 uintptr,
+	inputData uintptr,
+        inputLength uint32,
+	output1 uintptr,
+        output2 uintptr,
 
 	// The second set of arguments will be the ports for interacting with memory
 	memReadAddr chan<- axiprotocol.Addr,
@@ -32,51 +33,59 @@ func Top(
 	memWriteData chan<- axiprotocol.WriteData,
 	memWriteResp <-chan axiprotocol.WriteResp) {
 
-        x_arr_2 := [8]uint32{1,2,3,4,5,6,7,8}
-        x_arr := [8]uint32{1,2,3,4,5,6,7,8}
-        y_arr := [8]uint32{2,3,5,7,11,13,17,19}
+	inputChannel := make(chan uint32)
+	alphaResponse := make(chan int32)
+	betaResponse := make(chan int32)
 
-        var x_total uint32 = 0
-        var y_total uint32 = 0
+	go aximemory.ReadBurstUInt32(
+		memReadAddr, memReadData, true, inputData, inputLength, inputChannel)
 
-        for i:= 7; i != 0; i-- {
-            x_total = x_arr[i] + x_total
-        }
+        go func() {
 
-        for i:= 7; i != 0; i-- {
-            y_total = y_arr[i] + y_total
-        }
+            x_arr := [8]int32{1,2,3,4,5,6,7,8}
+            x_arr_2 := [8]int32{1,2,3,4,5,6,7,8}
+            y_arr := [8]int32{2,3,5,7,11,13,17,19}
 
-        // scale by 1024 so fractions are more precise
-        x_avg := 1024 * x_total / 8
-        y_avg := 1024 * y_total / 8
+            var x_total int32 = 0
+            var y_total int32 = 0
 
-        var beta1 uint32 = 0
+            for i:= inputLength - 1; i != 0; i-- {
+                x_total = x_arr[i] + x_total
+                y_total = y_arr[i] + y_total
+            }
 
-        for i := 7; i != 0; i-- {
-            beta1 = beta1 + (x_arr[i] * 1024 - x_avg) * (y_arr[i] * 1024 - y_avg)
-        }
+            // scale by 1024 so fractions are more precise. Divide by 8 because that's how many inputs we have
+            x_avg := (x_total << 10) / int32(inputLength)
+            y_avg := (y_total << 10) / int32(inputLength)
 
-        // can't multiply x_arr[i] by itself??
-        var beta2 uint32 = 0
-        for i := 7; i != 0; i-- {
-            beta2 = beta2 + (x_arr_2[i] * 1024 - x_avg) * (x_arr[i] * 1024 - x_avg)
-        }
+            var beta1 int32 = 0
+            var beta2 int32 = 0
 
-        beta := beta2 / beta1
+            for i := inputLength - 1; i != 0; i-- {
+                y_next := <- inputChannel
+                beta1 = beta1 + (x_arr[i] << 10 - x_avg) * (int32(y_next) << 10 - y_avg)
+                beta2 = beta2 + (x_arr_2[i] << 10 - x_avg) * (x_arr[i] << 10 - x_avg)
+            }
 
-        alpha := y_avg - beta * x_avg
+            beta := beta2 / beta1
 
-	// Calculate alpha
-	val1 := uint32(alpha)
+            alpha := y_avg - beta * x_avg
 
-        // Calculate beta
-        val2 := uint32(beta)
+            // Calculate alpha
+            alphaResponse <- alpha
 
-	// Write them back to the pointers the host requests
-	aximemory.WriteUInt32(
-		memWriteAddr, memWriteData, memWriteResp, false, addr1, val1)
-	aximemory.WriteUInt32(
-		memWriteAddr, memWriteData, memWriteResp, false, addr2, val2)
+            // Calculate beta
+            betaResponse <- beta
+
+        }()
+
+        val1 := <-alphaResponse
+        val2 := <-betaResponse
+
+        // Write them back to the pointers the host requests
+        aximemory.WriteUInt32(
+                memWriteAddr, memWriteData, memWriteResp, false, output1, uint32(val1))
+        aximemory.WriteUInt32(
+                memWriteAddr, memWriteData, memWriteResp, false, output2, uint32(val2))
 
 }
