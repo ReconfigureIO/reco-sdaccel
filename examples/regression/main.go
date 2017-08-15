095321eb4f8999c16346c9914a1e257fd1a18047
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-08-15 15:24:14 -0500

    refactor bits of the regression example

--- examples/regression/main.go
+++ examples/regression/main.go
@@ -41,6 +41,13 @@ func MakePair(c <-chan uint64) Pair {
 	}
 }
 
+type Result struct {
+    x_total int32
+    y_total int32
+    product_sum int32
+    squared_sum int32
+}
+
 func squared_sum_func(x [8]int32) int32 {
     squared_array := [8]int32{x[0] * x[0],
         x[1] * x[1],
@@ -53,6 +60,46 @@ func squared_sum_func(x [8]int32) int32 {
 
     return add_slice(squared_array)
 }
+
+func regression(inputLength uint32, inputChannel <-chan uint64) Result {
+
+    var x_total int32 = 0
+    var y_total int32 = 0
+
+    // scale by 1024 so fractions are more precise. Divide by 8 because that's how many inputs we have
+
+    // nΣxy
+    var product_sum int32 = 0
+
+    // nΣx^2
+    var squared_sum int32 = 0
+
+    var input_length_int int32 = 0
+    input_length_int = int32(inputLength)
+
+    var length int = 0
+    length = int(inputLength)/8
+
+    for ; length > 0; length-- {
+
+        x_arr := [8]int32{}
+        y_arr := [8]int32{}
+
+        for i := 1; i <= 8; i ++ {
+            next := int32(<- inputChannel)
+            x_arr[i-1] = int32(next >> 32)
+            y_arr[i-1] = int32(next)
+        }
+
+        product_sum = product_sum + input_length_int * product_sum_slice(x_arr, y_arr)
+        x_total = x_total + add_slice(x_arr)
+        y_total = y_total + add_slice(y_arr)
+        squared_sum = squared_sum + input_length_int * squared_sum_func(x_arr)
+
+    }
+    return Result { x_total: x_total, y_total: y_total, product_sum: product_sum, squared_sum: squared_sum }
+}
+
 
 // The Top function will be presented as a kernel
 func Top(
@@ -76,87 +123,26 @@ func Top(
 	memWriteResp <-chan axiprotocol.WriteResp) {
 
         independentChannel := make(chan uint64)
-	inputChannel := make(chan uint32)
+	inputChannel := make(chan uint64)
 	alphaResponse := make(chan int32)
 	betaResponse := make(chan int32)
 
 	go aximemory.ReadBurstUInt64(
 		memReadAddr, memReadData, true, inputData, inputLength * 2, inputChannel)
 
-        // compute concurrently with reads.
-        go func() {
+            result := regression(inputLength, inputChannel)
 
-            var x_total int32 = 0
-            var y_total int32 = 0
-
-            // scale by 1024 so fractions are more precise. Divide by 8 because that's how many inputs we have
-
-            // nΣxy
-            var product_sum int32 = 0
-
-            // nΣx^2
-            var squared_sum int32 = 0
-
-            var input_length_int int32 = 0
-            input_length_int = int32(inputLength)
-
-            var length int = 0
-            length = int(inputLength)/8
-
-            for ; length > 0; length-- {
-
-                go func () {
-
-                    x_arr := [8]int32{}
-                    y_arr := [8]int32{}
-
-                    x_arr[0] = int32(<-inputChannel)
-                    y_arr[0] = int32(<-inputChannel)
-                    x_arr[1] = int32(<-inputChannel)
-                    y_arr[1] = int32(<-inputChannel)
-                    x_arr[2] = int32(<-inputChannel)
-                    y_arr[2] = int32(<-inputChannel)
-                    x_arr[3] = int32(<-inputChannel)
-                    y_arr[3] = int32(<-inputChannel)
-                    x_arr[4] = int32(<-inputChannel)
-                    y_arr[4] = int32(<-inputChannel)
-                    x_arr[5] = int32(<-inputChannel)
-                    y_arr[5] = int32(<-inputChannel)
-                    x_arr[6] = int32(<-inputChannel)
-                    y_arr[6] = int32(<-inputChannel)
-                    x_arr[7] = int32(<-inputChannel)
-                    y_arr[7] = int32(<-inputChannel)
-
-                    /*x_next := <- inputChannel
-                    y_next := <- inputChannel
-
-                    x := int32(x_next)
-                    y := int32(y_next)
-                    
-                    product_sum = product_sum + input_length_int * x * y
-                    x_total = x_total + x
-                    y_total = y_total + y
-                    squared_sum = squared_sum + input_length_int * x  * x*/
-
-                    product_sum = product_sum + input_length_int * product_sum_slice(x_arr, y_arr)
-                    x_total = x_total + add_slice(x_arr)
-                    y_total = y_total + add_slice(y_arr)
-                    squared_sum = squared_sum + input_length_int * squared_sum_func(x_arr)
-
-                }()
-            }
-
-            var x_avg int32 = x_total / int32(inputLength)
-            var y_avg int32 = y_total / int32(inputLength)
+            var x_avg int32 = result.x_total / int32(inputLength)
+            var y_avg int32 = result.y_total / int32(inputLength)
 
             // ΣxΣy
-            var pairwise_product int32 = x_total * y_total
+            var pairwise_product int32 = result.x_total * result.y_total
 
             // nΣxy - ΣxΣy
-            var beta2 int32 = product_sum - (x_total * y_total)
+            var beta2 int32 = result.product_sum - (result.x_total * result.y_total)
 
             // nΣx^2 - (Σx)^2
-            var beta1 int32 = squared_sum - (x_total * x_total)
+            var beta1 int32 = result.squared_sum - (result.x_total * result.x_total)
 
             // slope
             beta := beta2 << 10 / beta1
@@ -165,8 +151,6 @@ func Top(
 
             alphaResponse <- alpha
             betaResponse <- beta
-
-        }()
 
         val1 := <-alphaResponse
         val2 := <-betaResponse
