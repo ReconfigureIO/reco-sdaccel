004f511c515c691a83d29c880b048f5df25d2f22
Josh Bohde <josh@joshbohde.com>
2017-08-24 09:47:29 -0500

    Fix updater.

--- ci/update-builder.sh
+++ ci/update-builder.sh
@@ -3,7 +3,7 @@ set -ex
 VERSION="$1"
 BRANCH=bump/update-sdaccel-to-$VERSION
 git checkout -b "$BRANCH"
-sed -i "s/^SDACCEL_WRAPPER_VERSION\ :=.*/SDACCEL_WRAPPER_VERSION\ :=\ ${VERSION}/g" Makefile
+sed -i "s/^export SDACCEL_WRAPPER_VERSION\ :=.*/export SDACCEL_WRAPPER_VERSION\ :=\ ${VERSION}/g" Makefile
 sed -i "s/defaultValue:\ 'v*.*.*'/defaultValue:\ '${VERSION}'/g" Jenkinsfile
 git add Makefile Jenkinsfile
 git commit -m "[auto] update sdaccel wrapper to ${VERSION}"
