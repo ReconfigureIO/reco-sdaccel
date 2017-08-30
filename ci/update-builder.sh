0631bf01db27541992d8d8c1c1ac3f910ecba399
Josh Bohde <josh@joshbohde.com>
2017-08-30 10:39:57 -0500

    Fix version overriding.

--- ci/update-builder.sh
+++ ci/update-builder.sh
@@ -4,7 +4,6 @@ VERSION="$1"
 BRANCH=bump/update-sdaccel-to-$VERSION
 git checkout -b "$BRANCH"
 sed -i "s/^export SDACCEL_WRAPPER_VERSION\ :=.*/export SDACCEL_WRAPPER_VERSION\ :=\ ${VERSION}/g" Makefile
-sed -i "s/defaultValue:\ 'v*.*.*'/defaultValue:\ '${VERSION}'/g" Jenkinsfile
 git add Makefile Jenkinsfile
 git commit -m "[auto] update sdaccel wrapper to ${VERSION}"
 git push -u origin "$BRANCH"
