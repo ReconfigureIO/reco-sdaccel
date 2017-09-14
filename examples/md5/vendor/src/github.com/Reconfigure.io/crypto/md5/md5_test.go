204a743e90bfa59073f665ee8b83c14fecec3d62
Josh Bohde <josh@joshbohde.com>
2017-09-14 12:59:14 -0500

    quickcheck against main Go.

--- examples/md5/vendor/src/github.com/Reconfigure.io/crypto/md5/md5_test.go
+++ examples/md5/vendor/src/github.com/Reconfigure.io/crypto/md5/md5_test.go
@@ -2,10 +2,11 @@ package md5
 
 import (
 	"bytes"
+	"crypto/md5"
 	"encoding/binary"
 	"encoding/hex"
 	"testing"
-	//	"testing/quick"
+	"testing/quick"
 )
 
 func Pad(b []byte) []byte {
@@ -60,8 +61,24 @@ func ShouldBeReference(t *testing.T, message string, reference string) {
 	}
 }
 
-func TestBlock(t *testing.T) {
+func TestReference(t *testing.T) {
 	ShouldBeReference(t, "", "d41d8cd98f00b204e9800998ecf8427e")
 	ShouldBeReference(t, "The quick brown fox jumps over the lazy dog", "9e107d9d372bb6826bd81d3542a419d6")
 	ShouldBeReference(t, "The quick brown fox jumps over the lazy dog.", "e4d909c290d0fb1ca068ffaddf22cbd0")
+}
+
+func TestMain(t *testing.T) {
+	ours := func(bytes []byte) string {
+		b := Sum(bytes)
+		return hex.EncodeToString(b)
+	}
+
+	theirs := func(bytes []byte) string {
+		b := md5.Sum(bytes)
+		return hex.EncodeToString(b[:])
+	}
+
+	if err := quick.CheckEqual(ours, theirs, nil); err != nil {
+		t.Error(err)
+	}
 }
