abcd702225495b424705f6504936499f38d386d4
Josh Bohde <josh@joshbohde.com>
2017-09-14 12:56:31 -0500

    Add md5_test.go

---
+++ examples/md5/vendor/src/github.com/Reconfigure.io/crypto/md5/md5_test.go
@@ -0,0 +1,67 @@
+package md5
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"testing"
+	//	"testing/quick"
+)
+
+func Pad(b []byte) []byte {
+	len := uint64(len(b))
+
+	var buff bytes.Buffer
+	// add 1 bit
+	buff.Write([]byte{0x80})
+
+	// append "0" bit until message length in bits ≡ 448 (mod 512)
+	remaining := (len + 1) % 64
+	toWrite := 56 - remaining
+
+	if remaining >= 56 {
+		toWrite = 64 + toWrite
+	}
+
+	// pad with zeros
+	buff.Write(make([]byte, toWrite))
+
+	// write length in bits
+	binary.Write(&buff, binary.LittleEndian, len<<3)
+
+	return append(b, buff.Bytes()...)
+
+}
+
+func Sum(bs []byte) []byte {
+	b := Pad(bs)
+	buff := bytes.NewBuffer(b)
+
+	arr := [16]uint32{}
+
+	dig := New()
+
+	i := len(b) / 4
+	for i != 0 {
+		binary.Read(buff, binary.LittleEndian, &arr)
+		dig = dig.Block(arr)
+		i -= 16
+	}
+	ret := dig.Sum()
+	return ret[:]
+}
+
+func ShouldBeReference(t *testing.T, message string, reference string) {
+	b := Sum([]byte(message))
+
+	s := hex.EncodeToString(b)
+	if s != reference {
+		t.Errorf("Expected reference \"%s\" to be \"%s\" got \"%s\"", message, reference, s)
+	}
+}
+
+func TestBlock(t *testing.T) {
+	ShouldBeReference(t, "", "d41d8cd98f00b204e9800998ecf8427e")
+	ShouldBeReference(t, "The quick brown fox jumps over the lazy dog", "9e107d9d372bb6826bd81d3542a419d6")
+	ShouldBeReference(t, "The quick brown fox jumps over the lazy dog.", "e4d909c290d0fb1ca068ffaddf22cbd0")
+}
