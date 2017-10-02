8fb9e75596a3415372be966a1ff06d29cf26ff7e
Patrick Thomson <patrick.thomson@reconfigure.io>
2017-10-02 17:17:01 -0400

    no ls or pwd

--- ci/test.sh
+++ ci/test.sh
@@ -7,5 +7,4 @@ cd "$1"
 sdaccel-builder test-go
 docker run --rm -i -v "$PWD":/mnt verilator -Wall --lint-only -I".reco-work/sdaccel/verilog/includes" .reco-work/sdaccel/verilog/main.v --top-module sda_kernel_wrapper_gmem --report-unoptflat -Wno-UNDRIVEN
 docker run --rm -i -v "$PWD":/mnt sdaccel-builder:latest /opt/sdaccel-builder/sdaccel-builder graph
-ls; pwd
 test -f main-graph.pdf
