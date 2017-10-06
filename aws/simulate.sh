440ee183e5acb299a4165cf4c780047f96b95823
Josh Bohde <josh@joshbohde.com>
2017-10-06 14:13:38 -0500

    Handle windows generated archives.

--- aws/simulate.sh
+++ aws/simulate.sh
@@ -5,7 +5,8 @@ source "/opt/sdaccel-builder/settings.sh"
 curl -XPOST -H "Content-Type: application/json"  -d '{"status": "STARTED"}' "$CALLBACK_URL" &> /dev/null
 
 set +e
-aws s3 cp --quiet "$INPUT_URL" - | tar zxf -
+aws s3 cp --quiet "$INPUT_URL" - | tar zxf - --transform='s,\\,/,g' --show-transformed-names
+
 if [ $? -ne 0 ]; then
     exit="$?"
     curl -XPOST -H "Content-Type: application/json"  -d '{"status": "ERRORED"}' "$CALLBACK_URL" &> /dev/null
