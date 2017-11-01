9e25772ba4aae209f19b6fd6dcaaea8b22ab5bb9
Josh Bohde <josh@joshbohde.com>
2017-11-01 09:03:07 -0500

    Update to aws-fpga v1.3.3

--- amis/install_platform.sh
+++ amis/install_platform.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 set -x
-VERSION=v1.3.0-pre
+VERSION=1.3.3
 mkdir -p /tmp/aws-fpga
 cd /tmp/aws-fpga
-aws s3 cp --quiet s3://nerabus/platform/aws-fpga-preview-$VERSION.zip .
-unzip aws-fpga-preview-$VERSION.zip
-#cd aws-fpga-preview-master
-export XILINX_SDX=/opt/Xilinx/SDx/2017.1.op/
+aws s3 cp --quiet s3://nerabus/platform/aws-fpga-$VERSION.zip .
+unzip aws-fpga-$VERSION.zip
+cd aws-fpga-$VERSION
+export XILINX_SDX=/opt/Xilinx/SDx/2017.1.op
 export AWS_FPGA_REPO_DIR="$PWD"
 export SDK_DIR="$PWD/sdk"
 
-source $XILINX_SDX/settings.sh
+source $XILINX_SDX/settings64.sh
 cd sdk
 source sdk_install.sh
-cd SDAccel
+
+cd ..
 # This isn't correctly generated in sdaccel_setup.sh
-make ec2=1 -C userspace/src
-source sdaccel_setup.sh
-cp tools/create_sdaccel_afi.sh "$XILINX_SDX/bin"
-cd ../..
-cp -R ./sdk/SDAccel/aws_platform/xilinx_aws-vu9p-f1_4ddr-xpr-2pr_4_0/ "$XILINX_SDX/platforms"
+PATH=/usr/bin:$PATH source sdaccel_setup.sh
+cp SDAccel/tools/create_sdaccel_afi.sh "$XILINX_SDX/bin"
+cp -R SDAccel/aws_platform/xilinx_aws-vu9p-f1_4ddr-xpr-2pr_4_0/ "$XILINX_SDX/platforms"
 rm -rf /tmp/aws-fpga
