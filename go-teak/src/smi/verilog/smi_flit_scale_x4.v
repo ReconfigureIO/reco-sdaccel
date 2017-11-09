a51b06f67248354d85ae614618fbe18c37ea881d
zynaptic <chris@zynaptic.com>
2017-11-09 14:00:25 +0000

    Added support for flit width scaling expansion factors of 2, 4 and 8.

--- go-teak/src/smi/verilog/smi_flit_scale_x4.v
+++ go-teak/src/smi/verilog/smi_flit_scale_x4.v
@@ -5,13 +5,13 @@
 //
 
 //
-// Provides support for SMI flit width scaling. This variant supports doubling
-// of the input flit data width.
+// Provides support for SMI flit width scaling. This variant supports an
+// increase of the input flit data width by a factor of 4.
 //
 
 `timescale 1ns/1ps
 
-module smiFlitScaleX2
+module smiFlitScaleX4
   (smiInReady, smiInEofc, smiInData, smiInStop, smiOutReady, smiOutEofc,
   smiOutData, smiOutStop, clk, srst);
 
@@ -26,9 +26,6 @@ parameter FifoSize = 16;
 // the binary representation of FifoSize-1.
 parameter FifoIndexSize = 4;
 
-// Derives the mask for unused end of frame control bits.
-parameter EofcMask = 2 * FlitWidth - 1;
-
 // Specifies the clock and active high synchronous reset signals.
 input clk;
 input srst;
@@ -42,7 +39,7 @@ output                  smiInStop;
 // Specifies the SMI output signals.
 output                    smiOutReady;
 output [7:0]              smiOutEofc;
-output [FlitWidth*16-1:0] smiOutData;
+output [FlitWidth*32-1:0] smiOutData;
 input                     smiOutStop;
 
 // Specifies the SMI buffered input signals.
@@ -52,12 +49,18 @@ wire [FlitWidth*8-1:0] smiInBufData;
 wire                   smiInBufStop;
 wire [FlitWidth*8+7:0] smiInBufVec;
 
+// Specifies the internal connections.
+wire                    smiSc1Ready;
+wire [7:0]              smiSc1Eofc;
+wire [FlitWidth*16-1:0] smiSc1Data;
+wire                    smiSc1Stop;
+
 // Specifies the SMI bus width expansion signals.
-wire                    smiScReady;
-wire [7:0]              smiScEofc;
-wire [FlitWidth*16-1:0] smiScData;
-wire                    smiScStop;
-wire [FlitWidth*16+7:0] smiOutVec;
+wire                    smiSc2Ready;
+wire [7:0]              smiSc2Eofc;
+wire [FlitWidth*32-1:0] smiSc2Data;
+wire                    smiSc2Stop;
+wire [FlitWidth*32+7:0] smiOutVec;
 
 // Instantiate the data input FIFO.
 selfLinkBufferFifoS #(FlitWidth*8+8, FifoSize, FifoIndexSize) smiBufIn
@@ -67,17 +70,22 @@ selfLinkBufferFifoS #(FlitWidth*8+8, FifoSize, FifoIndexSize) smiBufIn
 assign smiInBufEofc = smiInBufVec [FlitWidth*8+7:FlitWidth*8];
 assign smiInBufData = smiInBufVec [FlitWidth*8-1:0];
 
-// Instantiate the flit scaling stage.
-smiFlitScaleStageX2 #(FlitWidth) scaleStage
-  (smiInBufReady, smiInBufEofc, smiInBufData, smiInBufStop, smiScReady,
-  smiScEofc, smiScData, smiScStop, clk, srst);
+// Instantiate the first stage scaling.
+smiFlitScaleStageX2 #(FlitWidth) scaleStage1
+  (smiInBufReady, smiInBufEofc, smiInBufData, smiInBufStop, smiSc1Ready,
+  smiSc1Eofc, smiSc1Data, smiSc1Stop, clk, srst);
+
+// Instantiate the second stage scaling.
+smiFlitScaleStageX2 #(FlitWidth*2) scaleStage2
+  (smiSc1Ready, smiSc1Eofc, smiSc1Data, smiSc1Stop, smiSc2Ready, smiSc2Eofc,
+  smiSc2Data, smiSc2Stop, clk, srst);
 
 // Instantiate the data output buffer.
-selfLinkToggleBuffer #(FlitWidth*16+8) smiBufOut
-  (smiScReady, {smiScEofc, smiScData}, smiScStop, smiOutReady, smiOutVec,
+selfLinkToggleBuffer #(FlitWidth*32+8) smiBufOut
+  (smiSc2Ready, {smiSc2Eofc, smiSc2Data}, smiSc2Stop, smiOutReady, smiOutVec,
   smiOutStop, clk, srst);
 
-assign smiOutEofc = smiOutVec[FlitWidth*16+7:FlitWidth*16];
-assign smiOutData = smiOutVec[FlitWidth*16-1:0];
+assign smiOutEofc = smiOutVec[FlitWidth*32+7:FlitWidth*32];
+assign smiOutData = smiOutVec[FlitWidth*32-1:0];
 
 endmodule
--- go-teak/src/smi/verilog/smi_flit_scale_x8.v
+++ go-teak/src/smi/verilog/smi_flit_scale_x8.v
@@ -5,13 +5,13 @@
 //
 
 //
-// Provides support for SMI flit width scaling. This variant supports doubling
-// of the input flit data width.
+// Provides support for SMI flit width scaling. This variant supports an
+// increase of the input flit data width by a factor of 8.
 //
 
 `timescale 1ns/1ps
 
-module smiFlitScaleX2
+module smiFlitScaleX8
   (smiInReady, smiInEofc, smiInData, smiInStop, smiOutReady, smiOutEofc,
   smiOutData, smiOutStop, clk, srst);
 
@@ -26,9 +26,6 @@ parameter FifoSize = 16;
 // the binary representation of FifoSize-1.
 parameter FifoIndexSize = 4;
 
-// Derives the mask for unused end of frame control bits.
-parameter EofcMask = 2 * FlitWidth - 1;
-
 // Specifies the clock and active high synchronous reset signals.
 input clk;
 input srst;
@@ -42,7 +39,7 @@ output                  smiInStop;
 // Specifies the SMI output signals.
 output                    smiOutReady;
 output [7:0]              smiOutEofc;
-output [FlitWidth*16-1:0] smiOutData;
+output [FlitWidth*64-1:0] smiOutData;
 input                     smiOutStop;
 
 // Specifies the SMI buffered input signals.
@@ -52,12 +49,23 @@ wire [FlitWidth*8-1:0] smiInBufData;
 wire                   smiInBufStop;
 wire [FlitWidth*8+7:0] smiInBufVec;
 
+// Specifies the internal connections.
+wire                    smiSc1Ready;
+wire [7:0]              smiSc1Eofc;
+wire [FlitWidth*16-1:0] smiSc1Data;
+wire                    smiSc1Stop;
+
+wire                    smiSc2Ready;
+wire [7:0]              smiSc2Eofc;
+wire [FlitWidth*32-1:0] smiSc2Data;
+wire                    smiSc2Stop;
+
 // Specifies the SMI bus width expansion signals.
-wire                    smiScReady;
-wire [7:0]              smiScEofc;
-wire [FlitWidth*16-1:0] smiScData;
-wire                    smiScStop;
-wire [FlitWidth*16+7:0] smiOutVec;
+wire                    smiSc3Ready;
+wire [7:0]              smiSc3Eofc;
+wire [FlitWidth*64-1:0] smiSc3Data;
+wire                    smiSc3Stop;
+wire [FlitWidth*64+7:0] smiOutVec;
 
 // Instantiate the data input FIFO.
 selfLinkBufferFifoS #(FlitWidth*8+8, FifoSize, FifoIndexSize) smiBufIn
@@ -67,17 +75,27 @@ selfLinkBufferFifoS #(FlitWidth*8+8, FifoSize, FifoIndexSize) smiBufIn
 assign smiInBufEofc = smiInBufVec [FlitWidth*8+7:FlitWidth*8];
 assign smiInBufData = smiInBufVec [FlitWidth*8-1:0];
 
-// Instantiate the flit scaling stage.
-smiFlitScaleStageX2 #(FlitWidth) scaleStage
-  (smiInBufReady, smiInBufEofc, smiInBufData, smiInBufStop, smiScReady,
-  smiScEofc, smiScData, smiScStop, clk, srst);
+// Instantiate the first stage scaling.
+smiFlitScaleStageX2 #(FlitWidth, FifoSize, FifoIndexSize) scaleStage1
+  (smiInBufReady, smiInBufEofc, smiInBufData, smiInBufStop, smiSc1Ready,
+  smiSc1Eofc, smiSc1Data, smiSc1Stop, clk, srst);
+
+// Instantiate the second stage scaling.
+smiFlitScaleStageX2 #(FlitWidth*2, FifoSize, FifoIndexSize) scaleStage2
+  (smiSc1Ready, smiSc1Eofc, smiSc1Data, smiSc1Stop, smiSc2Ready, smiSc2Eofc,
+  smiSc2Data, smiSc2Stop, clk, srst);
+
+// Instantiate the third stage scaling.
+smiFlitScaleStageX2 #(FlitWidth*4, FifoSize, FifoIndexSize) scaleStage3
+  (smiSc2Ready, smiSc2Eofc, smiSc2Data, smiSc2Stop, smiSc3Ready, smiSc3Eofc,
+  smiSc3Data, smiSc3Stop, clk, srst);
 
 // Instantiate the data output buffer.
-selfLinkToggleBuffer #(FlitWidth*16+8) smiBufOut
-  (smiScReady, {smiScEofc, smiScData}, smiScStop, smiOutReady, smiOutVec,
+selfLinkToggleBuffer #(FlitWidth*64+8) smiBufOut
+  (smiSc3Ready, {smiSc3Eofc, smiSc3Data}, smiSc3Stop, smiOutReady, smiOutVec,
   smiOutStop, clk, srst);
 
-assign smiOutEofc = smiOutVec[FlitWidth*16+7:FlitWidth*16];
-assign smiOutData = smiOutVec[FlitWidth*16-1:0];
+assign smiOutEofc = smiOutVec[FlitWidth*64+7:FlitWidth*64];
+assign smiOutData = smiOutVec[FlitWidth*64-1:0];
 
 endmodule
