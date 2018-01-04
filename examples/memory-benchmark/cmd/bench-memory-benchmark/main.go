8d5e243e2a1c57caa5dc2c11c9f11054faf1305e
Josh Bohde <josh@joshbohde.com>
2018-01-04 13:41:39 -0600

    Add first sketch of the hostside for memory benchmark.

---
+++ examples/memory-benchmark/cmd/bench-memory-benchmark/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
+	"testing"
+	"xcl"
+
+	"ReconfigureIO/reco-sdaccel/benchmarks"
+)
+
+type Result struct {
+	something uint32
+}
+
+const (
+	DATA_WIDTH = 1024
+	// Burst width in bytes
+	BURST_WIDTH = 8
+)
+
+func main() {
+	world := xcl.NewWorld()
+	defer world.Release()
+
+	program := world.Import("kernel_test")
+	defer program.Release()
+
+	krnl := program.GetKernel("reconfigure_io_sdaccel_builder_stub_0_1")
+	defer krnl.Release()
+
+	f := func(B *testing.B) {
+		doit(world, krnl, B)
+	}
+
+	bm := testing.Benchmark(f)
+	benchmarks.GipedaResults("memcopy", bm)
+}
+
+func doit(world xcl.World, krnl *xcl.Kernel, B *testing.B) {
+	B.SetBytes(BURST_WIDTH * B.N)
+	B.ReportAllocs()
+
+	input := make([]byte, byteLength)
+	_, err := rand.Read(input)
+
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	inputBuff := world.Malloc(xcl.WriteOnly, DATA_WIDTH)
+	defer inputBuff.Free()
+
+	var result Result
+
+	outputBuff := world.Malloc(xcl.WriteOnly, uint(binary.Size(result)))
+	defer outputBuff.Free()
+
+	burstCount := uint32(B.N)
+
+	krnl.SetMemoryArg(0, inputBuff)
+	// non inclusive upper limit of memory
+	//	krnl.SetMemoryArg(1, outputBuff)
+	krnl.SetArg(2, burstCount)
+	krnl.SetMemoryArg(3, outputBuff)
+
+	B.ResetTimer()
+	krnl.Run(1, 1, 1)
+	B.StopTimer()
+}
---
+++ examples/memory-benchmark/cmd/test-memory-benchmark/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/binary"
+	"log"
+	"xcl"
+)
+
+type Result struct {
+	something uint32
+}
+
+const DATA_WIDTH = 1024
+
+func main() {
+	world := xcl.NewWorld()
+	defer world.Release()
+
+	krnl := world.Import("kernel_test").GetKernel("reconfigure_io_sdaccel_builder_stub_0_1")
+	defer krnl.Release()
+
+	inputBuff := world.Malloc(xcl.WriteOnly, DATA_WIDTH)
+	defer inputBuff.Free()
+
+	var result Result
+
+	outputBuff := world.Malloc(xcl.WriteOnly, uint(binary.Size(result)))
+	defer outputBuff.Free()
+
+	burstCount := uint32(DATA_WIDTH / 64)
+
+	krnl.SetMemoryArg(0, inputBuff)
+	// non inclusive upper limit of memory
+	//	krnl.SetMemoryArg(1, outputBuff)
+	krnl.SetArg(2, burstCount)
+	krnl.SetMemoryArg(3, outputBuff)
+
+	krnl.Run(1, 1, 1)
+
+	err := binary.Read(outputBuff.Reader(), binary.LittleEndian, &result)
+
+	if err != nil {
+		log.Fatal("binary.Read failed:", err)
+	}
+
+	// TODO add some test to verify it ran
+	return true
+
+}
