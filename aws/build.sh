88b16c90dd57f0bf2d177b6bf4d26c92afd93cfc
Josh Bohde <josh@joshbohde.com>
2018-04-23 12:15:58 -0500

    Use bundled /opt/create_sdaccel_afi.sh

--- aws/build.sh
+++ aws/build.sh
@@ -45,7 +45,7 @@ cat times.out
 
 if [ "$GENERATE_AFI" = "yes" ]; then
     echo "generating afi"
-    create_sdaccel_afi.sh -s3_bucket="$DCP_BUCKET" -s3_dcp_key="$DCP_KEY" -s3_logs_key="$LOG_KEY" -xclbin=".reco-work/sdaccel/dist/xclbin/kernel_test.hw.$DEVICE.xclbin" -o=".reco-work/sdaccel/dist/xclbin/kernel_test.hw.$DEVICE"
+    /opt/create_sdaccel_afi.sh -s3_bucket="$DCP_BUCKET" -s3_dcp_key="$DCP_KEY" -s3_logs_key="$LOG_KEY" -xclbin=".reco-work/sdaccel/dist/xclbin/kernel_test.hw.$DEVICE.xclbin" -o=".reco-work/sdaccel/dist/xclbin/kernel_test.hw.$DEVICE"
     mv ".reco-work/sdaccel/dist/xclbin/kernel_test.hw.$DEVICE.xclbin" ".reco-work/sdaccel/dist/xclbin/kernel_test.hw.$DEVICE.xclbin.raw"
     mv ".reco-work/sdaccel/dist/xclbin/kernel_test.hw.$DEVICE.awsxclbin" ".reco-work/sdaccel/dist/xclbin/kernel_test.hw.$DEVICE.xclbin"
     AGFI=$(cat ./*_agfi_id.txt)
