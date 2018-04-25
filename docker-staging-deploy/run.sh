e264d438fe283b3ac8db853294ea0dfd733bc40e
Josh Bohde <josh@joshbohde.com>
2018-04-25 11:52:16 -0500

    Setup RTE for 1DDR.

--- docker-staging-deploy/run.sh
+++ docker-staging-deploy/run.sh
@@ -6,14 +6,29 @@ unzip /tmp/bundle.zip -d "$PWD"
 fpga-clear-local-image -S 0
 fpga-load-local-image -S 0 -I agfi-09c2a21805a8b9257
 fpga-clear-local-image -S 0
+
 fpga-describe-local-image -S 0 -H –R
 fpga-load-local-image -S 0 -I "$AGFI"
 fpga-describe-local-image -S 0 -H –R
 
-
-source "${XILINX_SDX}/settings64.sh"
-source "${XILINX_SDX%.op}.rte/setup.sh"
-export PATH=$PWD/.reco-work/sdaccel/dist/:$PATH
 export XCL_BINDIR="$PWD/.reco-work/sdaccel/dist/xclbin"
+
+# Set the default RTE
+export RTE="${XILINX_SDX%.op}.rte"
+
+# Check if we have a 1DDR, and load that RTE instead
+if compgen -G "$XCL_BINDIR/*-f1_1ddr-xpr-2pr_4_0.xclbin" > /dev/null; then
+    export RTE="${XILINX_SDX%.op}.rte.1ddr"
+fi
+
+# for some reason the .rte only accepts 1ddr formats, so rename them
+for file in "$XCL_BINDIR"/*.xclbin ; do mv "$file" "${file//f1_1ddr/f1_4ddr}"; done
+
+# Setup environment
+source "${XILINX_SDX}/settings64.sh"
+source "${RTE}/setup.sh"
+export PATH=$PWD/.reco-work/sdaccel/dist/:$PATH
+
+# This is in case they have anything in dist we want a relative path to
 cd "$PWD/.reco-work/sdaccel/dist"
 bash -c "$CMD"
