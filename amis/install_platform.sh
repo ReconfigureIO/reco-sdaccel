1999ea3ecba7311833c59ad18d66616bb1df8a60
Josh Bohde <josh@joshbohde.com>
2018-04-25 11:56:07 -0500

    Update AMI to symlink the SDx RTE for backwards compatability.

--- amis/install_platform.sh
+++ amis/install_platform.sh
@@ -20,4 +20,8 @@ PATH=/usr/bin:$PATH source sdaccel_setup.sh
 cp SDAccel/tools/create_sdaccel_afi.sh "$XILINX_SDX/bin"
 cp -R SDAccel/aws_platform/xilinx_aws-vu9p-f1_4ddr-xpr-2pr_4_0/ "$XILINX_SDX/platforms"
 cp -R SDAccel/aws_platform/xilinx_aws-vu9p-f1_1ddr-xpr-2pr_4_0/ "$XILINX_SDX/platforms"
+
+# We depend on the bare 2017.1 rte, but the ami splits it out into 1ddr and 4ddr versions.
+# Symlink to keep backwards compatability
+ln -s /opt/Xilinx/SDx/2017.1.rte.4ddr/ /opt/Xilinx/SDx/2017.1.rte/
 rm -rf /tmp/aws-fpga
