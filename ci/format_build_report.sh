9016e24b395f690df6642e6caba1f7c21e70681f
Josh Bohde <josh@joshbohde.com>
2018-04-27 16:44:20 -0500

    memoryKB -> memory

--- ci/format_build_report.sh
+++ ci/format_build_report.sh
@@ -5,7 +5,7 @@ name="$1"
 inp=$(cat)
 
 # e.g. buildTime/verilog/histogram-parallel
-echo "$inp" | jq -r ".timing | .[] | [[\"buildTime/\" + .step + \"/$1\", .seconds], [\"memoryKB/\" + .step + \"/$1\", .kb]] | .[] | @tsv" | sed -e "s/\t/;/"
+echo "$inp" | jq -r ".timing | .[] | [[\"buildTime/\" + .step + \"/$1\", .seconds], [\"memory/\" + .step + \"/$1\", .kb]] | .[] | @tsv" | sed -e "s/\t/;/"
 
 # e.g. DSPs/used/histogram-parallel
 echo "$inp" | jq -r "[.lutSummary, .regSummary, .blockRamSummary, .ultraRamSummary, .dspBlockSummary] | .[] | [[.description + \"/used/$1\", .used], [.description + \"/percentage/$1\", .utilisation]] | .[] | @tsv" | sed -e "s/\t/;/"
