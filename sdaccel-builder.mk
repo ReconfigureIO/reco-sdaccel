1a5eb961dbc7b332310c9451bad227bd48679f18
Max Siegieda <maxsiegieda@gmail.com>
2018-09-17 15:42:46 +0100

    split sim and build reports into their own commands

--- sdaccel-builder.mk
+++ sdaccel-builder.mk
@@ -81,10 +81,15 @@ ${BUILD_DIR}/reports/timing.json: | ${BUILD_DIR}/reports
 ${BUILD_DIR}/reports/data_clk_frequency.json: ${XCLBIN_DIR}/${KERNEL_NAME}.${TARGET}.${DEVICE}.xclbin | ${BUILD_DIR}/reports
 	extract_data_clk_frequency ${XCLBIN_DIR}/${KERNEL_NAME}.${TARGET}.${DEVICE}.xclbin > $@
 
-${REPORTS_DIR}/utilization.json: ${BUILD_DIR}/reports/timing.json ${BUILD_DIR}/reports/data_clk_frequency.json | ${REPORTS_DIR}
+${REPORTS_DIR}/build_report.json: ${BUILD_DIR}/reports/timing.json ${BUILD_DIR}/reports/data_clk_frequency.json | ${REPORTS_DIR}
 	merge_reports ${BUILD_DIR}/reports/reconfigure_io_sdaccel_builder_stub_0_1_util.json ${BUILD_DIR}/reports/data_clk_frequency.json ${BUILD_DIR}/reports/timing.json > $@
 
-report: ${REPORTS_DIR}/utilization.json
+${REPORTS_DIR}/sim_report.json: ${BUILD_DIR}/reports/timing.json | ${REPORTS_DIR}
+	merge_reports ${BUILD_DIR}/reports/reconfigure_io_sdaccel_builder_stub_0_1_util.json ${BUILD_DIR}/reports/timing.json > $@
+
+report: ${REPORTS_DIR}/build_report.json
+
+sim_report: ${REPORTS_DIR}/sim_report.json
 
 ${XCLBIN_DIR}/${KERNEL_NAME}.${TARGET}.${DEVICE}.xclbin: ${BUILD_DIR}/${XO_NAME} ${XCLBIN_DIR}
 	cd ${BUILD_DIR} && /usr/bin/time -ao ${ROOT_DIR}/times.out -f "xclbin,%e,%M" xocc -j${CPUS} -O3 -t "${TARGET}" $(CLFLAGS) --xdevice ${DEVICE_FULL} -l $< -o $@ -r system
