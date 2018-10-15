3cd76c8c3c0d1a325588e0cb8cf701593525f90f
Chris Holgate <chris@zynaptic.com>
2018-10-15 11:28:12 +0100

    Corrected target for vendor source directory rule.

--- sdaccel-builder.mk
+++ sdaccel-builder.mk
@@ -122,8 +122,7 @@ ${DIST_DIR}/emconfig.json: ${DIST_DIR}
 
 sim: ${DIST_DIR}/emconfig.json
 
-fix:
-	${DIST_DIR}/vendor/src/.fixed
+fix: ${VENDOR_DIR}/.fixed
 
 ${DIST_DIR}:
 	mkdir -p "${DIST_DIR}"
@@ -135,19 +134,19 @@ ${REPORTS_DIR}:
 ${LOGS_DIR}:
 	mkdir -p "${LOGS_DIR}"
 
-${DIST_DIR}/vendor/src: | ${ROOT_DIR}/vendor/github.com/ReconfigureIO/sdaccel
+${VENDOR_DIR}/src: | ${ROOT_DIR}/vendor/github.com/ReconfigureIO/sdaccel
 	mkdir -p "${VENDOR_DIR}"
 	ln -sfT "${ROOT_DIR}/vendor" "${VENDOR_DIR}/src"
 
-${DIST_DIR}/vendor/src/.fixed: | ${DIST_DIR}/vendor/src
+${VENDOR_DIR}/.fixed: | ${VENDOR_DIR}/src
 	${DIR}/go/bin/reco-fix .
-	touch ${DIST_DIR}/vendor/src/.fixed
+	touch ${VENDOR_DIR}/.fixed
 
 ${ROOT_DIR}/vendor/github.com/ReconfigureIO/sdaccel:
 	mkdir -p ${ROOT_DIR}/vendor/github.com/ReconfigureIO
 	ln -sf "${DIR}/go/src/github.com/ReconfigureIO/sdaccel" ${ROOT_DIR}/vendor/github.com/ReconfigureIO/sdaccel
 
-${DIST_DIR}/%: ${ROOT_DIR}/cmd/%/main.go ${DIST_DIR} | ${DIST_DIR}/vendor/src/.fixed
+${DIST_DIR}/%: ${ROOT_DIR}/cmd/%/main.go ${DIST_DIR} | ${VENDOR_DIR}/.fixed
 	LIBRARY_PATH=${XILINX_SDX}/SDK/lib/lnx64.o/:${XILINX_SDX}/runtime/lib/x86_64/:/usr/lib/x86_64-linux-gnu/:${LIBRARY_PATH} \
 	CGO_CFLAGS=-I${XILINX_SDX}/runtime/include/1_2/ \
 	GOPATH=${VENDOR_DIR}:${DIR}/go \
@@ -164,7 +163,7 @@ ${VERILOG_DIR}:
 VERILOG_SOURCES := $(shell find ${DIR}/eTeak/verilog/SELF_files/ -type f)
 INCLUDE_TARGETS := $(patsubst ${DIR}/eTeak/verilog/SELF_files/%,${VERILOG_DIR}/includes/%,$(VERILOG_SOURCES))
 
-${VERILOG_DIR}/main.v: ${ROOT_DIR}/${SOURCE_FILE} $(INCLUDE_TARGETS) | ${VERILOG_DIR} ${DIST_DIR}/vendor/src/.fixed
+${VERILOG_DIR}/main.v: ${ROOT_DIR}/${SOURCE_FILE} $(INCLUDE_TARGETS) | ${VERILOG_DIR} ${VENDOR_DIR}/.fixed
 ifeq ($(INPUT),go)
 	/usr/bin/time -ao ${ROOT_DIR}/times.out -f "verilog,%e,%M" build_go $< $@
 else
@@ -172,7 +171,7 @@ else
 	cp ${ROOT_DIR}/main.v.xmldef ${VERILOG_DIR}
 endif
 
-${ROOT_DIR}/main-graph.pdf: ${ROOT_DIR}/main.go $(INCLUDE_TARGETS) | ${VERILOG_DIR} ${DIST_DIR}/vendor/src/.fixed
+${ROOT_DIR}/main-graph.pdf: ${ROOT_DIR}/main.go $(INCLUDE_TARGETS) | ${VERILOG_DIR} ${VENDOR_DIR}/.fixed
 	cd ${DIR}/eTeak && \
 	PATH=${DIR}/eTeak/bin:${PATH} \
 	GOPATH=${VENDOR_DIR} \
