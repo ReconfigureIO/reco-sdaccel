9843ad167ee3b225c2f493c4d6acb036cd9f7d2c
Chris Holgate <chris@zynaptic.com>
2018-12-06 12:09:34 +0000

    Updated to latest AWS-F1 SDK version and required Vivado version

--- amis/install_platform.sh
+++ amis/install_platform.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -x
-VERSION=1.3.6d
+VERSION=1.4.5
 mkdir -p /tmp/aws-fpga
 cd /tmp/aws-fpga
 wget -O aws-fpga-$VERSION.zip https://github.com/aws/aws-fpga/archive/v$VERSION.zip
 unzip aws-fpga-$VERSION.zip
 cd aws-fpga-$VERSION
-export XILINX_SDX=/opt/Xilinx/SDx/2017.1.op
+export XILINX_SDX=/opt/Xilinx/SDx/2017.4.op
 export AWS_FPGA_REPO_DIR="$PWD"
 export SDK_DIR="$PWD/sdk"
 
@@ -20,7 +20,7 @@ PATH=/usr/bin:$PATH source sdaccel_setup.sh
 cp -R SDAccel/aws_platform/xilinx_aws-vu9p-f1_4ddr-xpr-2pr_4_0/ "$XILINX_SDX/platforms"
 cp -R SDAccel/aws_platform/xilinx_aws-vu9p-f1_1ddr-xpr-2pr_4_0/ "$XILINX_SDX/platforms"
 
-# We depend on the bare 2017.1 rte, but the ami splits it out into 1ddr and 4ddr versions.
+# We depend on the bare 2017.4 rte, but the ami splits it out into 1ddr and 4ddr versions.
 # Symlink to keep backwards compatability
-ln -s /opt/Xilinx/SDx/2017.1.rte.4ddr /opt/Xilinx/SDx/2017.1.rte
+ln -s /opt/Xilinx/SDx/2017.4.rte.4ddr /opt/Xilinx/SDx/2017.4.rte
 rm -rf /tmp/aws-fpga
