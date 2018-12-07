ac41273ca28d5fba597f282d8345ce5c80c81812
Max Siegieda <maxsiegieda@gmail.com>
2018-12-07 12:35:14 +0000

    Switch to new shell naming scheme

--- amis/install_platform.sh
+++ amis/install_platform.sh
@@ -6,7 +6,7 @@ cd /tmp/aws-fpga
 wget -O aws-fpga-$VERSION.zip https://github.com/aws/aws-fpga/archive/v$VERSION.zip
 unzip aws-fpga-$VERSION.zip
 cd aws-fpga-$VERSION
-export XILINX_SDX=/opt/Xilinx/SDx/2017.4.op
+export XILINX_SDX=/opt/Xilinx/SDx/2018.2.op
 export AWS_FPGA_REPO_DIR="$PWD"
 export SDK_DIR="$PWD/sdk"
 
@@ -17,10 +17,6 @@ source sdk_install.sh
 cd ..
 # This isn't correctly generated in sdaccel_setup.sh
 PATH=/usr/bin:$PATH source sdaccel_setup.sh
-cp -R SDAccel/aws_platform/xilinx_aws-vu9p-f1_4ddr-xpr-2pr_4_0/ "$XILINX_SDX/platforms"
-cp -R SDAccel/aws_platform/xilinx_aws-vu9p-f1_1ddr-xpr-2pr_4_0/ "$XILINX_SDX/platforms"
+cp -R SDAccel/aws_platform/xilinx_aws-vu9p-f1-04261818_dynamic_5_0/ "$XILINX_SDX/platforms"
 
-# We depend on the bare 2017.4 rte, but the ami splits it out into 1ddr and 4ddr versions.
-# Symlink to keep backwards compatability
-ln -s /opt/Xilinx/SDx/2017.4.rte.4ddr /opt/Xilinx/SDx/2017.4.rte
 rm -rf /tmp/aws-fpga
