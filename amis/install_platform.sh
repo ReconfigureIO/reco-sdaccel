7d69e2cdc55f54bbb00da068328d03f77ac7627d
Peter Waller <p@pwaller.net>
2018-12-11 15:42:55 +0000

    Use set -eux in install_platform.sh

--- amis/install_platform.sh
+++ amis/install_platform.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
-set -x
+set -eux
+
 VERSION=1.4.5
+
 mkdir -p /tmp/aws-fpga
 cd /tmp/aws-fpga
-wget -O aws-fpga-$VERSION.zip https://github.com/aws/aws-fpga/archive/v$VERSION.zip
-unzip aws-fpga-$VERSION.zip
+
+wget -q -O aws-fpga-$VERSION.zip https://github.com/aws/aws-fpga/archive/v$VERSION.zip
+unzip -q aws-fpga-$VERSION.zip
 cd aws-fpga-$VERSION
+
 export XILINX_SDX=/opt/Xilinx/SDx/2018.2.op2258646
 export AWS_FPGA_REPO_DIR="$PWD"
 export SDK_DIR="$PWD/sdk"
 
-source $XILINX_SDX/settings64.sh
-cd sdk
-source sdk_install.sh
+{
+    # Beware, must disable `set -eu` for running code from elsewhere...
+    set +eu
 
-cd ..
-# This isn't correctly generated in sdaccel_setup.sh
-PATH=/usr/bin:$PATH source sdaccel_setup.sh
+    source $XILINX_SDX/settings64.sh
+
+    cd sdk
+    source sdk_install.sh
+
+    cd ..
+    # This isn't correctly generated in sdaccel_setup.sh
+    PATH=/usr/bin:$PATH source sdaccel_setup.sh
+
+    set -eu
+}
+
 cp -R SDAccel/aws_platform/xilinx_aws-vu9p-f1-04261818_dynamic_5_0/ "$XILINX_SDX/platforms"
 
 rm -rf /tmp/aws-fpga
