d06d08e861648434f5b6db49609fe9ec6bf2acc3
Peter Waller <p@pwaller.net>
2018-12-17 11:02:30 +0000

    Rewrite install_platform.sh (include /opt/Xilinx/aws-fpga)
    * Include the aws-fpga project in /opt/Xilinx so it is available at
      runtime.
    * Rewrite script to put configurables at the top.
    * Include many more comments about what is going on.
    * Use long form command line arguments where available.
    * Drop redundant sdk_install.sh call (included via sdaccel_setup.sh).
    
    Old state:
    
    We used to only take a temporary copy of aws-fpga, and copy out the
    development kit, deleting it when done. However, AWS now requires that
    sdaccel_setup.sh has been sourced in order to do AFI generation.
    Therefore, put it in /opt/Xilinx, so that it takes advantage of the
    existing bind mounts and is available at runtime inside our builder
    docker containers.
    
    We used to source sdk_install.sh. This seems obviously incorrect -
    according to that script it is not meant to be sourced. It is also run
    as a part of sdaccel_setup.sh, which sources sdk_setup.sh, and
    eventually runs sdk_install.sh, so it looks unnecessary.

--- amis/install_platform.sh
+++ amis/install_platform.sh
@@ -1,35 +1,54 @@
 #!/bin/bash
 set -eux
 
-VERSION=1.4.5
+# This script runs during our AMI build process run with "hashicorp packer".
 
-mkdir -p /tmp/aws-fpga
-cd /tmp/aws-fpga
+# This script grabs the AWS FPGA development repository and runs the install
+# process. The important effects of this script are:
 
-wget -q -O aws-fpga-$VERSION.zip https://github.com/aws/aws-fpga/archive/v$VERSION.zip
-unzip -q aws-fpga-$VERSION.zip
-cd aws-fpga-$VERSION
+# * To obtain the "vivado platform" and install it into the
+#   /opt/Xilinx/SDx/platforms directory.
+# * To put the aws-fpga repository at /opt/Xilinx/aws-fpga, so that
+#   sdaccel_setup.sh can be sourced during our AFI build.
 
-export XILINX_SDX=/opt/Xilinx/SDx/2018.2.op2258646
-export AWS_FPGA_REPO_DIR="$PWD"
-export SDK_DIR="$PWD/sdk"
+# These variables need updating when we change the SDK/HDK version. RIO_ prefix
+# to avoid risk of interference with AWS environment variables since this script
+# shares its environment with AWS scripts.
+RIO_AWS_FPGA_VERSION=1.4.5
+RIO_SDX_VERSION=2018.2.op2258646
+RIO_SDX_AWS_PLATFORM=xilinx_aws-vu9p-f1-04261818_dynamic_5_0
 
+export XILINX_SDX=/opt/Xilinx/SDx/${RIO_SDX_VERSION}
+
+# Grab the aws-fpga repository and put it in /opt/Xilinx/aws-fpga.
+# Location is not ideal, it is chosen because that is what is bind mounted into
+# our docker containers. Hopefully at some point we don't require this to be
+# available, or we move it to a more sensible location.
+{
+    wget \
+        --quiet \
+        --output-document=aws-fpga-${RIO_AWS_FPGA_VERSION}.zip \
+        https://github.com/aws/aws-fpga/archive/v${RIO_AWS_FPGA_VERSION}.zip
+
+    unzip -q aws-fpga-${RIO_AWS_FPGA_VERSION}.zip
+
+    mv ./aws-fpga-${RIO_AWS_FPGA_VERSION}/ /opt/Xilinx/aws-fpga
+}
+
+# Run sdaccel_setup.sh, which has a side effect of downloading the platforms
+# from the internet and setting various environment variables (which we don't
+# about at this point).
+cd /opt/Xilinx/aws-fpga
 {
     # Beware, must disable `set -eu` for running code from elsewhere...
     set +eu
 
-    source $XILINX_SDX/settings64.sh
-
-    cd sdk
-    source sdk_install.sh
-
-    cd ..
-    # This isn't correctly generated in sdaccel_setup.sh
-    PATH=/usr/bin:$PATH source sdaccel_setup.sh
+    source sdaccel_setup.sh
 
     set -eu
 }
 
-cp -R SDAccel/aws_platform/xilinx_aws-vu9p-f1-04261818_dynamic_5_0/ "$XILINX_SDX/platforms"
-
-rm -rf /tmp/aws-fpga
+# Copy the platform to the /opt/Xilinx/SDx/platforms/<platform name> directory.
+cp --recursive \
+    "SDAccel/aws_platform/${RIO_SDX_AWS_PLATFORM}/" \
+    "$XILINX_SDX/platforms/"
